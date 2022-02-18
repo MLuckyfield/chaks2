aae7bbdbac05432e043f82b98485aac353c63a6a
Mluckyfield <matt.cyoshida@gmail.com>
2022-02-18 16:47:22 +0900

    updated content, mobile navbar, added concept section

--- front/src/App.js
+++ front/src/App.js
@@ -6,12 +6,14 @@ import axios from 'axios';
 //import styles
 import './scss/main.scss'
 import logo from './chatshack.jpeg'
+import shunsuke from './shunsuke.jpg'
+import matt from './matt.jpg'
 
 const App = () => {
 
       const [text, setText] = useState([
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  'Have you ever wondered these things? After 5 years of English language education in public school, and maybe even after graduating from expensive Eikaiwa, many people still do not have the confidence to speak English well. Though people often have good knowledge of reading/wriitng, listening is harder and speaking is hardest. So eventually pople say "I need more opportunities to speak English!" Perhaps you tried language exchange apps, or meetups, to make new foreign friends. But what happens? Do they tell you everytime you make a mistake, or explain the mistake, or a better way to say it depending on the situation, or whats more natural? Maybe once a meeting! Usually, as long as they understand you, the conversation just carries on, and you never learn from that moment. Because the truth is, most people we meet through those channels are not there to really give you the feedback you need. So in summary: school (possibly the best option, but typically very expensive, and if structure is too formal it takes time to learn to speak naturally), meetups/exchange apps (very cheap and exciting in the beginning, but difficult to find a good one. Difficult to have someone remember your person patterns or guide your growth), or making new friends (free! but usually you just end up playing not learning))'
+  'このような悩みありませんか？高校や大学で英語を学んでも、高い英会話スクールに通っていたとしても、英語を話すことに自信を持つのは難しいでしょう。また、英語の読み書きには自信があっても、リスニングとスピーキングが苦手な方が多いかと思います。きっとそれは、気軽に楽しく英語を話す機会が少ないからなのではないでしょうか？語学交流アプリなどを利用して外語人の友人を作り、英語を話す機会を作ったとしても、きっと彼らはあなたの英語の間違いを１つ１つ丁寧に指摘や修正はしてくれないでしょう。'
       ])
 
       const email = useRef('');
@@ -56,7 +58,7 @@ const App = () => {
                     <ul id="menu">
                       <li><img class='avatar' id='nav-logo' src={logo} alt="Avatar"></img><div class='logo-basic'>CHATSHACK</div></li>
                       <li><a href="#intro">Intro</a></li>
-                      <li><a href="#">Concept</a></li>
+                      <li><a href="#concept">Concept</a></li>
                       <li><a href="#merit">Merit</a></li>
                       <li><a href="#team">Team</a></li>
                     </ul>
@@ -72,27 +74,58 @@ const App = () => {
                         </div>
                     </div>
               </div>
-              <div id='intro' class='master-row'>
-              <div class='row '>
+              <div class='master-row'>
+              <div id='intro'  class='row '>
                 <div class='col slim center'>
-                    <h2>"I do not know how to say that"<br/>"I know I am speaking incorrectly but I do not know when"<br/>"How can I send more natural"<br/>"What is the difference between these words"<br/>"When is this word appropriate"</h2>
+                    <div class='col slim center'>
+                    <h2>「英語でなんて表現すれば良いかわからない」<br/><br/>「自分の英語のどこが間違っているんだろう」<br/><br/>「正しい発音がわからない」</h2>
+
+                    </div>
                     <p/>{text[1]}
                 </div>
+              </div>
+              <div id='concept'  class='master-row dark'>
+                <div class='row'><h1 class='col'>CONCEPT</h1></div>
+                <div class='row'>
+                  <div class='col slim center'>
+                  CHATSHACKでは、これらの問題を解決することができます。
+会話はインストラクターがリードし、分からない英語については１つ１つ説明し、お客様が理解した上で、会話を続けます。なので、英会話に自信がない人や、正しく自然な英語を学びたい人は是非ともお試しください。
+                  </div>
+                </div>
+                <div class='row'>
+                    <div class='col slim center border'>
+                    AFTERNOON
+                        <h2>ENGLISH + CAFE</h2>
+                        <p/>ゆっくりできるカフェスペースにて、自由に英会話を楽しめます！会話はインストラクターがリードするので、不安な方でも楽しくご利用いただけます。
+                        <p/>FREE! coffee or tea
+                        <p/>1000yen/30min
+                        <p/>10AM - 6PM
+                    </div>
+                    <div class='col slim center border'>
+                    EVENING
+                        <h2>ENGLISH + BAR</h2>
+                        <p/>季節のクラフトビールや、自家製のソーセージをつまみながら英会話を楽しめます！外国のバーにいるような雰囲気で楽しくインストラクターがあなたの英語をレベルアップしてくれます。
+                        <p/>FREE! 1 drink
+                        <p/>2500yen/UNLIMITED
+                        <p/>6PM - 12AM
+                    </div>
+                </div>
+                <div class='row'><p/></div>
               </div>
               <div id='merit' class='master-row mid'>
                   <div class='row'><h1 class='col'>MERIT</h1></div>
                   <div class='row'>
                         <div class='col slim'>
                             <span class='material-icons'>school</span>
-                            <div class='col'>{text[0]}</div>
+                            <div class='col'>自由なテーマで楽しめる！テキストを使わず、自由な会話をベースとしたスタイルなので、あなたの知りたいことや話したいことをテーマにして英会話を楽しめます。</div>
                         </div>
                         <div class='col slim'>
                             <span class='material-icons'>more_time</span>
-                            <div class='col'>{text[0]}</div>
+                            <div class='col'>いつでも参加できる！好きな時間に来店し自由に英会話へ参加ができ、好きな時間に退出ができます！</div>
                         </div>
-                        <div class='slim'>
+                        <div class='col slim'>
                             <span class='material-icons'>savings</span>
-                            <div class='col'>{text[0]}</div>
+                            <div class='col'>入会費、テキスト代などの費用が要らない！他社英会話教室などには入会金やテキスト代等ございますが、当店は入場料のお支払のみでご参加いただけます！</div>
                         </div>
                   </div>
               </div>
@@ -104,8 +137,8 @@ const App = () => {
                             <h1 class='col'>SUBSCRIBE</h1>
                         </div>
                         <div class='row'>
-                          <input ref={first} class='form-control' placeholder='First Name'/>
-                          <input ref={last} class='form-control' placeholder='Family Name'/>
+                          <input ref={first} class='form-control' minlength='1' placeholder='First Name'/>
+                          <input ref={last} class='form-control' minlength='1' placeholder='Family Name'/>
                         </div>
                         <div class='row'>
                           <input ref={email} class='form-control' type='email' placeholder='Email'/>
@@ -123,18 +156,18 @@ const App = () => {
                     <h1 class='col'>TEAM</h1>
                     <div class='row center'>
                         <div class='col slim'>
-                            <img class='avatar' src="https://thumbs.dreamstime.com/z/man-profile-cartoon-smiling-vector-illustration-graphic-design-135443492.jpg" alt="Avatar"></img>
+                            <img class='avatar' src={shunsuke}></img>
                             <h2>SHUNSUKE</h2>
                             <h3>CO-FOUNDER</h3>
-                            <span class='subtitle'>Legal, Sourcing, F&B, Funding, Communications</span>
-                            {text[0]}
+                            <span class='subtitle'>Legal, Supply Chain, Food & Beverage, Funding, External Communications</span>
+                            Born in Japan. Speaks 3 language. Lived in Canada and China. Likes travelling and has friends all over the world!
                         </div>
                         <div class='col slim'>
-                            <img class='avatar' src="https://thumbs.dreamstime.com/z/man-profile-cartoon-smiling-vector-illustration-graphic-design-135443492.jpg" alt="Avatar"></img>
+                            <img class='avatar' src={matt}></img>
                             <h2>MATT</h2>
                             <h3>CO-FOUNDER</h3>
-                            <span class='subtitle'>Research, Product, Branding & Marketing, Finance, Tech</span>
-                            {text[0]}
+                            <span class='subtitle'> Research, Product, Branding & Marketing, Finance, HR, Tech</span>
+                            Born in Canada. Speaks 1.5 languages. Lived in 5 countries. Has too many hobbies & interests - at least 5!
                         </div>
                     </div>
                 </div>
