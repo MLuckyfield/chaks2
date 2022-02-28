3673225385727f28b093e96f6d81875ff46c87ed
Mluckyfield <matt.cyoshida@gmail.com>
2022-02-28 14:51:29 +0900

    new navbar test

--- front/src/App.js
+++ front/src/App.js
@@ -38,7 +38,25 @@ const App = () => {
             setMsg([err.message,err.success]);
           });
       }
+      const navSlide=()=>{
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navLinks = document.querySelectorAll(".nav-links li");
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
 
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+
+      }
       return (
         // <Router>
         //     <Route path="/" component={Navbar}/>
@@ -49,20 +67,22 @@ const App = () => {
         //     </AuthDataProvider>
         // </Router>
         <div>
-              <nav role="navigation">
-                <div id="menuToggle">
-                  <input type="checkbox" />
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                    <ul id="menu">
-                      <li><img class='avatar' id='nav-logo' src={logo} alt="Avatar"></img><div class='logo-basic'>CHATSHACK</div></li>
-                      <li><a href="#intro">Intro</a></li>
-                      <li><a href="#concept">Concept</a></li>
-                      <li><a href="#merit">Merit</a></li>
-                      <li><a href="#team">Team</a></li>
-                    </ul>
-                </div>
+              
+              <nav>
+                  <div class="logo">
+                      <h4>The Nav</h4>
+                  </div>
+                  <ul class="nav-links">
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Work</a></li>
+                      <li><a href="#">Projects</a></li>
+                  </ul>
+                  <div class="burger" onClick={navSlide}>
+                      <div class="line1"></div>
+                      <div class="line2"></div>
+                      <div class="line3"></div>
+                  </div>
               </nav>
               <div id='header' class='transparent'>
                     <div id='overlay'>
