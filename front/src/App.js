6e36f37b8ba4b19851c361f02f67ae9dba748f03
Mluckyfield <matt.cyoshida@gmail.com>
2022-03-04 16:28:02 +0900

    css upgrade

--- front/src/App.js
+++ front/src/App.js
@@ -249,7 +249,7 @@ const Map = ()=>{
   },[])
   return (
     <div id='map'>
-        {location?<GoogleMapReact bootstrapURLKeys={{key:'AIzaSyBX-HH0dhkemDet_G5TTZsR__uphcOEI6k'}} defaultCenter={location} defaultZoom={15}>
+        {location?<GoogleMapReact bootstrapURLKeys={{key:'AIzaSyBX-HH0dhkemDet_G5TTZsR__uphcOEI6k'}} defaultCenter={location} defaultZoom={14}>
             <LocationPin lat={location.lat} lng={location.lng} text='ChatShack'/>
         </GoogleMapReact>:'loading'}
     </div>
