9ff1d1f812db1b34893785719f2791c4aaa47668
Mluckyfield <matt.cyoshida@gmail.com>
2022-05-25 23:48:18 +0900

    debug

--- front/src/components/utilities/table.js
+++ front/src/components/utilities/table.js
@@ -9,7 +9,7 @@ const Table = (props)=> {
   const [message, setMessage] = useState();
 
   useEffect(() => {
-    if(data!==null){
+    if(data==null){
       axios.get(props.api, {params:{filter:props.filter,fields:props.fields}})
         .then((res) => {
             console.log(res.data.data)
