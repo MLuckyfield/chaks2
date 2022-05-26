05e98f4dd4ab5346cb16b1ece5e153c012d31f66
Mluckyfield <matt.cyoshida@gmail.com>
2022-05-26 21:43:40 +0900

    debug

--- front/src/components/utilities/booking.js
+++ front/src/components/utilities/booking.js
@@ -7,12 +7,14 @@ const Booking = ()=>{
 
   const [day, setDay]=useState(new Date())
   const [msg,setMsg] = useState()
+  const [bookings, setBookings]=useState()
+  const [student, setStudent]=useState(JSON.parse(localStorage.getItem('user'))._id)
 
   const onSubmit=(e)=>{
     e.preventDefault();
     axios.post('/booking/new',
       {
-        student: JSON.parse(localStorage.getItem('user'))._id,
+        student: student,
         date: day,
       })
       .then((res) => {
@@ -24,17 +26,29 @@ const Booking = ()=>{
         });
   }
   const prepBooking=(data)=>{
-    
+
     setDay(data)
   }
   useEffect(()=>{
-
+    axios.get('/booking/all', {params:{filter:student}})
+      .then((res) => {
+          setBookings(res.data.data.reverse());
+        })
+      .catch(error => console.log("error"+error))
   },[])
 
   return (
     <div class='master-row'>
       <h1>Reservations</h1>
           <div class='col'>
+              {bookings ? (bookings.map(function(item, i){
+                  return (
+                    <div class='col feedback'>
+                        <div class=''>{item.date}</div>
+                    </div>
+                  )
+
+                })): 'No reservations. Why not make one? :)'}
               <form class='login' onSubmit={onSubmit}>
                   <div class="form-group">
                   {msg?<div class='row'><input class={msg[1]?'msg form-control':'bad msg form-control'} value={msg[0]}></input></div>  :''}
