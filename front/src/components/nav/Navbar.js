f2bea697a4c5f56baff9b18ccdb925de4289abc4
Mluckyfield <matt.cyoshida@gmail.com>
2022-05-26 17:30:48 +0900

    debug

--- front/src/components/nav/Navbar.js
+++ front/src/components/nav/Navbar.js
@@ -59,6 +59,7 @@ const Navbar = ()=>{
         ):(
           <ul class="nav-links">
               <li><a href='#' onClick={logout}><span class="material-icons">logout</span></a></li>
+              {user.role!='user'?(<li><a href='/dash'><span class="material-icons">group</span></a></li>):''}
           </ul>
         )}
         <div class="burger" onClick={navSlide}>
