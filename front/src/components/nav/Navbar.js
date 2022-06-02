c1c3521b59bc3ad7dedf5a1feff776545ccfe68d
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-02 13:46:27 +0900

    login button

--- front/src/components/nav/Navbar.js
+++ front/src/components/nav/Navbar.js
@@ -55,7 +55,7 @@ const Navbar = ()=>{
               <li><a onClick={navSlide} href="#access">ACCESS</a></li>
               <li><a onClick={navSlide} href="#faq">FAQ</a></li>
               <li><a onClick={navSlide} href="#team">TEAM</a></li>
-              <button class='form-control solid-first' onClick={()=>window.location('/login')}>Log In</button>
+              <li class='form-control solid-first' onClick={()=>window.location('/login')}>Log In</li>
           </ul>
         ):(
           <ul class="nav-links">
