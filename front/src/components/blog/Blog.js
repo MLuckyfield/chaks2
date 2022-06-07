c42388203cd1c2a75da88b39ddab66ea516cf039
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-07 21:58:00 +0900

    add blog

--- front/src/components/blog/Blog.js
+++ front/src/components/blog/Blog.js
@@ -19,7 +19,7 @@ const Blog = () => {
       {
         title: title.current.value,
         preview: preview.current.value,
-        content: convertToRaw(editorState),
+        content: convertToRaw(editorState.content.getCurrentContent()),
         author: author,
         date: new Date(),
       })
