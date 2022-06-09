fde65d97584524ddee03f59688fad3e748a5d65c
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-09 16:20:55 +0900

    add sns

--- front/src/App.js
+++ front/src/App.js
@@ -224,7 +224,7 @@ const Front = ()=>{
                     </div>
                   </form>
                 </div>
-                <div class='row'>
+                <div class='fixed-row'>
                   <div class='col'><img class='nav-logo' onClick={()=>window.location.href='https://instagram.com/chatshack/'} src={instagram} alt="English education and event information!"></img></div>
                   <div class='col'><img class='nav-logo' onClick={()=>window.location.href='https://twitter.com/CHATSHACK_Tokyo'} src={twitter} alt="Newest event and schedule information here!"></img></div>
                   <div class='col'><img class='nav-logo' onClick={()=>window.location.href='https://www.youtube.com/channel/UCjGUSfvKKj72blxyqusTRRg'} src={youtube} alt="English entertainment!"></img></div>
