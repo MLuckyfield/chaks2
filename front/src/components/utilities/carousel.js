f6191e4dc58aa1d3a12bcd8d16fd3d0fb11a8a76
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-10 18:14:50 +0900

    add carousel

--- front/src/components/utilities/carousel.js
+++ front/src/components/utilities/carousel.js
@@ -1,6 +1,4 @@
-import twitter from '../../sns_logos/twitter_dark.png'
-import youtube from '../../sns_logos/yt_dark.png'
-import instagram from '../../sns_logos/insta_dark.jpg'
+import banner from '../../banner.jpg'
 
 const Carousel = (props)=>{
 
