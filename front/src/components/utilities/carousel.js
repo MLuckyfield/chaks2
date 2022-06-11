00381feed0f919dd8b334220edaf0584977a0fcc
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-11 11:16:26 +0900

    add carousel

--- front/src/components/utilities/carousel.js
+++ front/src/components/utilities/carousel.js
@@ -7,13 +7,16 @@ const Carousel = (props)=>{
   const [current,setCurrent]=useState(0)
   let revolutions = 0
   const timer = (callback)=>{
+    console.log('timer called')
     for(let i=0;i<items.length-1;i++){
+      console.log('testing '+i)
       if(items[i].active){
+        console.log(items[i].text + ' is active')
         items[i].active=false
         items[next(i,items.length)].active=true
         setItems(items)
         break
-      }
+      }else{console.log(items[i].text+' is not active')}
     }
     if(revolutions<100){setInterval(timer,3000);revolutions++}
   }
