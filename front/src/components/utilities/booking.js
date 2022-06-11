8005b46ea8b4ec0dfe2f4f86abf5aee3231c9883
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-11 22:28:50 +0900

    fix calendar

--- front/src/components/utilities/booking.js
+++ front/src/components/utilities/booking.js
@@ -58,7 +58,7 @@ const Booking = ()=>{
 
                 })): 'No reservations. Why not make one? :)'}
                   {msg?<div class='row'><input class={msg[1]?'msg form-control':'bad msg form-control'} value={msg[0]}></input></div>  :''}
-                    <form>
+                    <form style={{width:'100%'}}>
                       <div class='form-group'>
                         <input type="text" class="form-control" placeholder={day?`${day}`:'Please pick a time'} disabled/>
                       </div>
