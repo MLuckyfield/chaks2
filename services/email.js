78c1fce52315da9c63aa8d39b3ce9ba80d63b16e
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-16 11:36:39 +0900

    mailchimp

--- services/email.js
+++ services/email.js
@@ -7,43 +7,55 @@ router.post('/internal_reservation', async (req, res)=>{
   console.log('starting email service')
   //SEND TO MAILCHIMP
   const email = 'support@chatshack.jp';
-  const addData = {
-      members: [
-         {
-            email_address: email,
-            status: "subscribed"
-         }
-      ]
-  }
-  addDataJson = JSON.stringify(addData);
-
-  const options = {
-      url: "https://us9.api.mailchimp.com/3.0/lists/cb86e9b6f5/members",
-      method: "POST",
-      headers: {
-          Authorization: `auth ${process.env.MAILCHIMP_AUTH}`
-      },
-      body: addDataJson
-  }
-  console.log('mailchimo request ready')
-  request (options, (error, response, body) => {
-      console.log(body)
-      // console.log(response)
-      body = JSON.parse(body)
-      if(body.errors) {
-        console.log(email+' failed to save to Mailchimp') // error :(
-          return res.status(500).json({
-            message: `user creation unsuccessful: ${body.errors}`,
+  // const addData = {
+  //     members: [
+  //        {
+  //           email_address: email,
+  //           status: "subscribed"
+  //        }
+  //     ]
+  // }
+  // addDataJson = JSON.stringify(addData);
+  //
+  // const options = {
+  //     url: "https://us9.api.mailchimp.com/3.0/lists/cb86e9b6f5/members",
+  //     method: "POST",
+  //     headers: {
+  //         Authorization: `auth ${process.env.MAILCHIMP_AUTH}`
+  //     },
+  //     body: addDataJson
+  // }
+  console.log('mailchimo request ready: '+email)
+  request({
+    url: 'https://us9.api.mailchimp.com/3.0/lists/cb86e9b6f5/members',
+    json: {
+        'email_address': email,
+        'user': `anystring: ${process.env.MAILCHIMP_AUTH}`,
+        'status': 'subscribed',
+        // 'merge_fields': {
+        //     'FNAME': user.firstName,
+        //     'LNAME': ''
+        // }
+    },
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `apikey ${process.env.MAILCHIMP_AUTH}`
+    }
+}, function(error, response, body){
+      if (error) {
+          return res.status(403).json({
+            message: 'email attempt failed:' +error,
             success: false
           });
-      } else {
-          console.log(email+' saved to Mailchimp') // error :(
-            return res.status(201).json({
-              message: 'User created',
-              success: true
-            });
-      }
-   })
+        } else {
+          console.log('Email sent');
+          return res.status(200).json({
+            message: 'Welcome back',
+            success: true
+          });
+        }
+    });
    //----mailchimp code ends here
   // console.log(req.body)
   // const transporter = nodemailer.createTransport({
