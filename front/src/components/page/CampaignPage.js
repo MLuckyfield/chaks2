4c311d2717fe53cbba7f3795b7c9146050fbc365
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-16 21:25:25 +0900

    add campaign

--- front/src/components/page/CampaignPage.js
+++ front/src/components/page/CampaignPage.js
@@ -7,10 +7,24 @@ const CampaignPage = () => {
 
   return(
     <div>
+      <div id='concept'  class='master-row' style={{background:'yellow',color:'white'}}>
+        <div class='row'><h1 class='col'>初回無料！！</h1></div>
+        <div class='row'>
+            <div class='col slim'>
+              <ol>
+                <li>CHATSHACKのSNSをフォロー</li>
+                <li>HPにてメールアドレスを登録</li>
+                <li>間無制限のFREEトライアル！</li>
+                ※飲食物の注文は別途費用が掛かります
+                ※１回のみ利用可能
+              </ol>
+            </div>
+        </div>
+      </div>
       <div id='concept'  class='master-row dark'>
         <div class='row'><h1 class='col'>INSTAGRAM</h1></div>
         <div class='row'>
-            <div class='col slim center border'>    
+            <div class='col slim'>
               <ol>
                 <li>@chatshackをフォロー</li>
                 <li>@chatshackのタグを付けて投稿</li>
@@ -19,12 +33,20 @@ const CampaignPage = () => {
                 ※１回のみ利用可能
               </ol>
             </div>
-            <div class='row'>
-              *月曜日は休みでございます
-            </div>
         </div>
-        <div class='row'><p/></div>
       </div>
+      <div class='row'>
+        <div class='col slim'>
+          <ol>
+            <li>入店時に友人と来たことを知らせる</li>
+            <li>次回の注文時に使えるカードをもらう</li>
+            <li>カードとお好きなドリンクを交換！</li>
+            ※クラフトビールはSサイズに限ります
+　　         ※１回のみ利用可能
+          </ol>
+        </div>
+      </div>
+      <AccessDisplay/>
     </div>
 )
 }
