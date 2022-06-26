156b3d992f18e18f67d5f002db0e9cc52d4739b2
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-26 18:32:46 +0900

    add pay

--- front/src/components/utilities/payment.js
+++ front/src/components/utilities/payment.js
@@ -74,39 +74,18 @@ const CheckoutForm = (props) => {
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
-
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    const {error} = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: 'https://example.com/order/123/complete',
-      },
-    });
-
-
-    if (error) {
-      // This point will only be reached if there is an immediate error when
-      // confirming the payment. Show error to your customer (for example, payment
-      // details incomplete)
-      setErrorMessage(error.message);
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
+    console.log(event)
   };
 
   return (
-    <div>payment</div>
+    <Elements stripe={stripe} options={options}>
+        <form onSubmit={handleSubmit}>
+          <PaymentElement />
+          <button disabled={!stripe}>Submit</button>
+          {/* Show error message to your customers */}
+          {errorMessage && <div>{errorMessage}</div>}
+        </form>
+    </Elements>
   )
 };
 export default CheckoutForm;
