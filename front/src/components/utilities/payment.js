f5c9846376acb92845769ed3d2ddb6a47739631f
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-26 18:58:16 +0900

    add pay

--- front/src/components/utilities/payment.js
+++ front/src/components/utilities/payment.js
@@ -54,27 +54,51 @@ const CheckoutForm = (props) => {
 
   const stripe = useStripe();
   const elements = useElements();
-  const [options,setOptions]=useState(()=>{
+  const [options,setOptions]=useState()
+  useEffect(()=>{
     axios.post('/payment/getId',{product:props.product})
       .then((res) => {
           console.log(res)
-              return {
-                  // passing the client secret obtained in step 2
-                  clientSecret: res.data.data.client_secret,
-                  // Fully customizable with appearance API.
-                  appearance: {/*...*/},
-              }
+          setOptions({clientSecret: res.data.data.client_secret})
           })
       .catch((err) => {
-        console.log(err)
-        return null
+          console.log(err)
         // setFeedback(err.response.data.message);
         });
-  })
+  },[])
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = async (event) => {
-    console.log(event)
+
+      // We don't want to let default form submission happen here,
+      // which would refresh the page.
+      event.preventDefault();
+
+      if (!stripe || !elements) {
+        // Stripe.js has not yet loaded.
+        // Make sure to disable form submission until Stripe.js has loaded.
+        return;
+      }
+
+      const {error} = await stripe.confirmPayment({
+        //`Elements` instance that was used to create the Payment Element
+        elements,
+        confirmParams: {
+          return_url: 'https://example.com/order/123/complete',
+        },
+      });
+
+
+      if (error) {
+        // This point will only be reached if there is an immediate error when
+        // confirming the payment. Show error to your customer (for example, payment
+        // details incomplete)
+        setErrorMessage(error.message);
+      } else {
+        // Your customer will be redirected to your `return_url`. For some payment
+        // methods like iDEAL, your customer will be redirected to an intermediate
+        // site first to authorize the payment, then redirected to the `return_url`.
+      }
   };
 
   return (
