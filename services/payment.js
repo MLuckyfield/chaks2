2925d1ffc7c5c8ba8152969991bee775993dd29b
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-29 15:17:37 +0900

    add pay

--- services/payment.js
+++ services/payment.js
@@ -28,7 +28,7 @@ router.post('/complete', express.raw({type:'application/json'}),async (req, res)
           console.log(session.id)
           stripe.checkout.sessions.retrieve(session.id,{expand:['line_items.data.price.product']},(err,checkout)=>{
             console.log(checkout.line_items.data)
-            console.log(checkout.line_items.data.price)
+            console.log(typeof checkout.line_items.data)
             console.log(checkout.line_items.data.id)
 
             // console.log(paymentIntent)
