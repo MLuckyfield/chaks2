817b955984054facbb370c96914b977457edc6d4
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-30 19:46:00 +0900

    add pay

--- services/payment.js
+++ services/payment.js
@@ -23,7 +23,7 @@ router.post('/complete', express.raw({type:'application/json'}),async (req, res)
       switch (event.type) {
         case 'checkout.session.completed':
           const session = event.data.object;
-          console.log(session)
+          console.log(session.subscription)
           // console.log(session.metadata)
           // console.log(session.id)
           stripe.checkout.sessions.retrieve(session.id,{expand:['line_items.data.price.product']},(err,checkout)=>{
@@ -34,21 +34,27 @@ router.post('/complete', express.raw({type:'application/json'}),async (req, res)
             if('points' in metadata){
               purchased = {$inc:{points:metadata.points * checkout.line_items.data[0].quantity}}
             }else if('plan' in metadata){
-              purchased = {plan:metadata.plan}
+              purchased = {
+                  plan:metadata.plan,
+                  stripe:{
+                    id:session.subscription,
+                    date:new Date()
+                  }
+              }
             }else if('sub_points'){purchased = {$inc:{points:metadata.sub_points * checkout.line_items.data[0].quantity}}}
             console.log('Order complete for: '+session.metadata.order)
-            // User.findByIdAndUpdate(session.metadata.order,purchased,{new:true}).then((result)=>{
-            //      console.log(result)
-            //         return res.status(201).json({
-            //           message: 'Booking saved',
-            //           success: true
-            //         });
-            //     }).catch((err)=>{
-            //         return res.status(501).json({
-            //           message: 'Booking saved',
-            //           success: false
-            //         });
-            //     })
+            User.findByIdAndUpdate(session.metadata.order,purchased,{new:true}).then((result)=>{
+                 console.log(result)
+                    return res.status(201).json({
+                      message: 'Booking saved',
+                      success: true
+                    });
+                }).catch((err)=>{
+                    return res.status(501).json({
+                      message: 'Booking saved',
+                      success: false
+                    });
+                })
           });
 
           // await stripe.checkout.sessions.listLineItems(
