af56e0871ab226069bb273174ca48a12bb3ad13b
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-30 14:36:57 +0900

    add pay

--- front/src/components/utilities/product_display.js
+++ front/src/components/utilities/product_display.js
@@ -30,7 +30,10 @@ const Product_Display = (props)=>{
                   <ul>
                     <li>予約不要で少人数のグループレッスン！
 入会費、テキスト代はなしで、来たいときに滞在した分だけの支払い！</li>
-                    <li><strong>レッスン時間</strong>30分~</li>
+                    <li><strong>英会話カフェ</strong>（10時～18時）
+¥1,000/30分（コーヒー、紅茶は飲み放題）</li>
+                    <li><strong>英会話バー</strong>（18時～23時）
+¥2,500/無制限（ワンドリンク付）</li>
                   </ul>
                 </div>
                 <div class="btn">
@@ -48,12 +51,12 @@ const Product_Display = (props)=>{
                 <div class="entry-content">
                   <ul>
                     <li>マンツーマン英会話で朝活をしましょう！ビジネス英語、TOEFL対策などお客様の希望に沿ったレッスンを提供します！</li>
-                    <li><strong>利用可能時間</strong>7時～10時</li>
-                    <li><strong>レッスン時間</strong>45分~</li>
+                    <li><strong>利用可能時間</strong> 7時～10時</li>
+                    <li><strong>レッスン時間</strong> 45分~</li>
                     <li style={{fontSize:'10px'}}><strong>※支払方法：ポイント制</strong> ポイントはマンツーマンレッスンを予約する際に利用します。100ポイントの購入から可能です。100ポイント=1レッスン</li>
                   </ul>
                 </div>
-                <div class="btn" onClick={(e)=>{setSelected('price_1LDexPBVAfieqaobsYFR70Im');onSubmit(e,'price_1LDexPBVAfieqaobsYFR70Im')}}>購入</div>
+                <div class="btn" onClick={(e)=>{onSubmit(e,'price_1LDexPBVAfieqaobsYFR70Im')}}>購入</div>
               </div>
             </div>
             <div class="plan ultimite">
@@ -70,14 +73,14 @@ const Product_Display = (props)=>{
 通常の英会話カフェ＆バーでのグループレッスンを時間制限なしで受講し放題！
 これから留学を考えている方や、外資系への転職を考えている人などへ向けたプランです！</li>
                     <li>
-                        <strong>利用可能時間</strong>10時～23時
+                        <strong>利用可能時間</strong> 10時～23時
                         <br/>
                         最終入店時間: 22時
                     </li>
-                    <li><strong>レッスン時間</strong>無制限~</li>
+                    <li><strong>レッスン時間</strong> 無制限~</li>
                   </ul>
                 </div>
-                <div class="btn" onClick={(e)=>{setSelected('price_1LDp5RBVAfieqaob33XKdjl2');onSubmit(e)}}>購入</div>
+                <div class="btn" onClick={(e)=>{onSubmit(e,'price_1LDp5RBVAfieqaob33XKdjl2')}}>購入</div>
               </div>
             </div>
           </div>
