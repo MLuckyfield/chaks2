81385921d9e87671e836ee5bcd1b77e7f8754361
Mluckyfield <matt.cyoshida@gmail.com>
2022-06-30 12:01:58 +0900

    add pay

--- front/src/components/utilities/product_display.js
+++ front/src/components/utilities/product_display.js
@@ -9,60 +9,35 @@ const Product_Display = (props)=>{
             <div class="plan">
               <div class="plan-inner">
                 <div class="entry-title">
-                  <h3>Basic Wash</h3>
-                  <div class="price">$25<span>/PER CAR</span>
+                  <h3>グループ</h3>
+                  <div class="price">¥330~<span>/60分</span>
                   </div>
                 </div>
                 <div class="entry-content">
                   <ul>
-                    <li><strong>1x</strong> option 1</li>
-                    <li><strong>2x</strong> option 2</li>
-                    <li><strong>3x</strong> option 3</li>
-                    <li><strong>Free</strong> option 4</li>
-                    <li><strong>Unlimited</strong> option 5</li>
+                    <li>予約不要で少人数のグループレッスン！
+入会費、テキスト代はなしで、来たいときに滞在した分だけの支払い！</li>
+                    <li><strong>レッスン時間</strong>30分~</li>
                   </ul>
                 </div>
                 <div class="btn">
-                  <a href="#">Order Now</a>
-                </div>
-              </div>
-            </div>
-            <div class="plan basic">
-              <div class="plan-inner">
-                <div class="hot">hot</div>
-                <div class="entry-title">
-                  <h3>Express Wash</h3>
-                  <div class="price">$50<span>/PER CAR</span>
-                  </div>
-                </div>
-                <div class="entry-content">
-                  <ul>
-                    <li><strong>1x</strong> option 1</li>
-                    <li><strong>2x</strong> option 2</li>
-                    <li><strong>3x</strong> option 3</li>
-                    <li><strong>Free</strong> option 4</li>
-                    <li><strong>Unlimited</strong> option 5</li>
-                  </ul>
-                </div>
-                <div class="btn">
-                  <a href="#">Order Now</a>
+                  <a href="#">店頭でのお支払い（カード払い可能）</a>
                 </div>
               </div>
             </div>
             <div class="plan standard">
               <div class="plan-inner">
                 <div class="entry-title">
-                  <h3>Super Wash</h3>
-                  <div class="price">$75<span>/PER CAR</span>
+                  <h3>マンツーマン</h3>
+                  <div class="price">¥5,000円<span>/45分</span>
                   </div>
                 </div>
                 <div class="entry-content">
                   <ul>
-                    <li><strong>2x</strong> Free Entrance</li>
-                    <li><strong>Free</strong> Snacks</li>
-                    <li><strong>Custom</strong> Swags</li>
-                    <li><strong>2x</strong> Certificate</li>
-                    <li><strong>Free</strong> Wifi</li>
+                    <li>マンツーマン英会話で朝活をしましょう！ビジネス英語、TOEFL対策などお客様の希望に沿ったレッスンを提供します！</li>
+                    <li>利用可能時間：7時～10時</li>
+                    <li><strong>レッスン時間</strong>45分~</li>
+                    <li><strong>※支払方法：ポイント制</strong> ポイントはマンツーマンレッスンを予約する際に利用します。100ポイントの購入から可能です。100ポイント=1レッスン</li>
                   </ul>
                 </div>
                 <div class="btn">
@@ -72,18 +47,25 @@ const Product_Display = (props)=>{
             </div>
             <div class="plan ultimite">
               <div class="plan-inner">
+              <div class="hot">週3回以上ならお得！</div>
                 <div class="entry-title">
-                  <h3>Unlimited Wash</h3>
-                  <div class="price">$100<span>/PER CAR</span>
+                  <h3>英会話放題</h3>
+                  <div class="price">¥30,000円<span>/月</span>
                   </div>
                 </div>
                 <div class="entry-content">
                   <ul>
-                    <li><strong>1x</strong> option 1</li>
-                    <li><strong>2x</strong> option 2</li>
-                    <li><strong>3x</strong> option 3</li>
-                    <li><strong>Free</strong> option 4</li>
-                    <li><strong>Unlimited</strong> option 5</li>
+                    <li>英語力の飛躍的向上を目指している方は通い放題コース！
+通常の英会話カフェ＆バーでのグループレッスンを時間制限なしで受講し放題！
+これから留学を考えている方や、外資系への転職を考えている人などへ向けたプランです！</li>
+                    <li>
+                        <ul>
+                            <li>利用可能時間：10時～23時</li>
+                            <li>最終入店時間:22時</li>
+                        </ul>
+                    </li>
+                    <li><strong>レッスン時間</strong>無制限~</li>
+                    <li><strong>最終入店時間</strong> 22時</li>
                   </ul>
                 </div>
                 <div class="btn">
