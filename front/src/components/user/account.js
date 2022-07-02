650350277e28ad73137c488d877436f777dd14d3
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-02 16:06:26 +0900

    add pages

--- front/src/components/user/account.js
+++ front/src/components/user/account.js
@@ -7,24 +7,24 @@ const Account = () => {
 
   //const [Account, setAccount] = useState();
   //const [password, setPassword] = useState();
-  const [student, setStudent] = useState(JSON.parse(localStorage.getItem('student')));
+  const [student, setStudent] = useState(JSON.parse(localStorage.getItem('user')));
   const [account,setAccount]=useState()
 
-  // useEffect(()=>{
-  //   console.log('loading account view for '+student)
-  //   axios.get('user/all', {params:{filter:{id:student._id},fields:'stripe'}})
-  //     .then((res) => {
-  //         // res.data.data.forEach((item, i) => {
-  //         //   item['inClass']=false
-  //         // });
-  //         console.log(res.data.data)
-  //         setAccount(res.data.data)
-  //       })
-  //     .catch(error => console.log("error"+error))
-  // },[])
-  // const onSubmit=(e)=>{
-  //
-  // }
+  useEffect(()=>{
+    console.log('loading account view for '+student)
+    axios.get('user/all', {params:{filter:{id:student._id},fields:'stripe'}})
+      .then((res) => {
+          // res.data.data.forEach((item, i) => {
+          //   item['inClass']=false
+          // });
+          console.log(res.data.data)
+          setAccount(res.data.data)
+        })
+      .catch(error => console.log("error"+error))
+  },[])
+  const onSubmit=(e)=>{
+
+  }
   return(
       <div class='row'>
           <h1>ACCOUNT</h1>
