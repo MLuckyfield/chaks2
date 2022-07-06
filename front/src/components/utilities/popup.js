cf9e606e8771bb48c330cbd3b0870c5e4ec657e4
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-06 17:29:59 +0900

    add tc

--- front/src/components/utilities/popup.js
+++ front/src/components/utilities/popup.js
@@ -6,7 +6,7 @@ const Popup = (props)=>{
 
   return (
     <div class="pop">
-      {props.button?<div for={props.num} class='btn'>購入</div>:<p><center><label for={props.num} class="open">{props.title}</label></center></p>}
+      {props.button?<div class='btn'><label for={props.num} class="open">{props.title}</label></div>:<p><center><label for={props.num} class="open">{props.title}</label></center></p>}
       <input type="checkbox" id={props.num} />
       <div class="modal">
         <div class="modal__inner">
