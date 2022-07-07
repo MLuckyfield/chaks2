31fd44319396b968a936f3ca836349cbd32a1c77
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-07 20:10:41 +0900

    add

--- front/src/components/utilities/booking.js
+++ front/src/components/utilities/booking.js
@@ -147,7 +147,7 @@ const Booking = ()=>{
                 })): 'No reservations. Why not make one? :)'}
                 <h2>Make a New Booking ({student?<span>Points {student.points}</span>:'0'})</h2><br/>
                     <Calendar onChange={updateView} value={day} minDate={date?date:new Date()}/>
-                    <select class='form-control' onChange={()=>{setAppointment(available[active])}} ref={active}>
+                    <select class='form-control' onChange={()=>{setAppointment(available[active.current.value])}} ref={active}>
                       {available?available.map(function(item,i){
                         return <option class='col slim feedback clickable' value={i}>{moment(item.slot).format('MMMM Do, h:mm a')} | TEACHER: {item.teacher} ({item.level})</option>
                       }):<option>No timeslots available!</option>}
