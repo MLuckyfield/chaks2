4a3cb50d6fa6e09cde2a4f6e379bfcf1a34eb007
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-07 17:55:00 +0900

    add

--- front/src/App.js
+++ front/src/App.js
@@ -78,7 +78,7 @@ const App = () => {
           <Route exact path="/blog" component={BlogFront}/>
           <Route exact path="/blog/*" component={BlogDisplay}/>
           <Route path="/products" component={Product_Display}/>
-          <AuthDataProvider>
+
             <SecureRoute path="/login" access={['user']} success={AdminDash} fail={Login}/>
             <SecureRoute path="/dash" access={['user','teacher','manager','admin']} success={AdminDash} fail={Login}/>
             <SecureRoute path="/account" access={['user','manager']} success={Account} fail={Login}/>
@@ -87,7 +87,7 @@ const App = () => {
             <SecureRoute path="/new-blog" access={['teacher','manager','admin']} success={Blog} fail={Login}/>
             <SecureRoute path="/update_profile" access={['teacher','manager','admin']} success={Profile} fail={Login}/>
             <SecureRoute path="/reservation" access={['user','teacher','manager']} success={Booking} fail={Login}/>
-          </AuthDataProvider>
+          
       </Router>
 
     )
