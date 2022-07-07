eec8a29bdde4055948be4e1e05cbfb36eb848005
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-07 14:02:50 +0900

    add signup

--- front/src/App.js
+++ front/src/App.js
@@ -74,7 +74,7 @@ const App = () => {
         </div>
           <Route exact path="/" component={Front}/>
           <Route path="/events" component={EventsPage}/>
-          <Route path="/signup" component={EventsPage}/>
+          <Route path="/signup" component={Signup}/>
           <Route path="/campaigns" component={CampaignPage}/>
           <Route path="/about" component={StylePage}/>
           <Route exact path="/blog" component={BlogFront}/>
