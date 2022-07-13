172e0e668b0d9060b186b8ac88d7b046d1ffce6b
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-13 13:53:36 +0900

    add

--- front/src/App.js
+++ front/src/App.js
@@ -89,8 +89,8 @@ const App = () => {
             <SecureRoute path="/reservations" access={['user','teacher','manager']} success={Booking} fail={Login}/>
             <SecureRoute path="/qr-reader" access={['user','teacher','manager']} success={()=><QrReader onResult={(result,error)=>{
               if(!!result){
-                // alert(JSON.stringify(result))
-                localStorage.setItem('student',result)                
+                alert(JSON.stringify(result))
+                localStorage.setItem('student',result)
                 window.location='/student'
               }
               if(!!error){
