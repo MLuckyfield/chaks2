6a7b1c96786bebc221f6e3116fce150e70841e53
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-19 17:15:24 +0900

    add

--- front/src/App.js
+++ front/src/App.js
@@ -261,16 +261,18 @@ const Front = ()=>{
                     </div>
                 </div>
               </div>
-              <div id='intro' class='master-row' style={{background:'#eebc21',color:'white',backgroundImage: 'url('+discount+')'}}>
-                <div class='col'>
-                    <div class='col slim center'>
-                        <h1 class='emphasize'>1時間0円!</h1>
-                        <h1>初回無料！</h1>
-                        <span style={{fontSize:'10px'}}>
-                            ※飲食物の注文は別途費用が掛かります
-                            ※１回のみ利用可能
-                        </span>
-                        <div class='col'><button class='solid-first cta' onClick={()=>window.location='/products'}>レッスン</button></div>
+              <div id='intro' class='master-row' style={{background:'#eebc21',color:'white',backgroundImage: 'url('+discount+')',backgroundSize:'cover'}}>
+                <div class='overlay' style={{background:'238,188,33,0.5'}}>
+                    <div class='col'>
+                        <div class='col slim center'>
+                            <h1 class='emphasize'>1時間0円!</h1>
+                            <h1>初回無料！</h1>
+                            <span style={{fontSize:'10px'}}>
+                                ※飲食物の注文は別途費用が掛かります
+                                ※１回のみ利用可能
+                            </span>
+                            <div class='col'><button class='solid-first cta' onClick={()=>window.location='/products'}>レッスン</button></div>
+                        </div>
                     </div>
                 </div>
               </div>
