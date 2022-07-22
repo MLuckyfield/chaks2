e65f31bf75e2dd556b821dbca7b83b8f4c6eaa44
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-22 12:46:18 +0900

    add

--- front/src/components/nav/SecureRoute.js
+++ front/src/components/nav/SecureRoute.js
@@ -1,26 +1,6 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import React, { useState, useEffect ,useRef} from 'react';
 
-const SecureRoute = ({ access, success, fail, ...options }) => {
-//const { user } = useAuthDataContext();
-
-let user = localStorage.getItem('user');
-if(user == '' || user == null || user == undefined){
-  return <Route {...options} component={fail} />;
-}else{
-  user = JSON.parse(localStorage.getItem('user'));
-  let okay = false
-  access.forEach((item, i) => {
-    if (user.role==item){okay=true}
-  });
-
-
-  if(okay==true){return <Route {...options} component={success} />;}
-  else{return <Route {...options} component={Redirect} />;}
-}
-
-
-}
 const Redirect = ()=>{
   useEffect(()=>{
     window.location='/login'
@@ -28,4 +8,24 @@ const Redirect = ()=>{
 
   return(<div></div>)
 }
+
+const SecureRoute = ({ access, success, fail, ...options }) => {
+//const { user } = useAuthDataContext();
+
+    let user = localStorage.getItem('user');
+    if(user == '' || user == null || user == undefined){
+      return <Route {...options} component={fail} />;
+    }else{
+      user = JSON.parse(localStorage.getItem('user'));
+      let okay = false
+      access.forEach((item, i) => {
+        if (user.role==item){okay=true}
+      });
+
+      if(okay==true){return <Route {...options} component={success} />;}
+      else{return <Route {...options} component={Redirect} />;}
+    }
+}
+
+
 export default SecureRoute;
