a4817fd478e6a651a99bbddabce9cd69a7bbf914
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-22 11:43:43 +0900

    add

--- front/src/components/nav/SecureRoute.js
+++ front/src/components/nav/SecureRoute.js
@@ -1,17 +1,18 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 const SecureRoute = ({ access, success, fail, ...options }) => {
 //const { user } = useAuthDataContext();
 
 let user = localStorage.getItem('user');
 if(user == '' || user == null || user == undefined){
-  window.location='/login'
+  return <Route {...options} component={Redirect} />
 }else{
   user = JSON.parse(localStorage.getItem('user'));
   access.forEach((item, i) => {
     if (user.role==item){return <Route {...options} component={success} />}
     else{
-      window.location='/login'
+      return <Route {...options} component={Redirect} />
     }
   });
 
@@ -19,4 +20,10 @@ if(user == '' || user == null || user == undefined){
 
 
 }
+const Redirect = ()=>{
+  useEffect(()=>{
+    window.location='/login'
+  },[])
+}
+
 export default SecureRoute;
