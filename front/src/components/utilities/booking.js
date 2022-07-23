38745180debaebf6cb9a23aa12907f1ca60c6d56
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-23 17:10:39 +0900

    add

--- front/src/components/utilities/booking.js
+++ front/src/components/utilities/booking.js
@@ -128,7 +128,8 @@ const Booking = ()=>{
   useEffect(()=>{
     axios.get('/booking/all', {params:{filter:{student:student,date:{$gte:new Date()}}}})
       .then((res) => {console.log('data okay')
-          setBookings(res.data.data.reverse());console.log('bookings okay')
+          // setBookings(res.data.data.reverse());
+          console.log('bookings okay')
           if(bookings){console.log('activating updateView for initial render');updateView(date)}
         })
       .catch(error => console.log("error"+error))
