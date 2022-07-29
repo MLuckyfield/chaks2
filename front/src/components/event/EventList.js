ed7382e46f90756f62679450bd672decf790aa5b
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-29 16:46:45 +0900

    add

--- front/src/components/event/EventList.js
+++ front/src/components/event/EventList.js
@@ -41,7 +41,7 @@ const EventList = () => {
       <div>
       {events?
         <span>
-        <div id='header' class='transparent' style={{backgroundImage: 'url('+require(events[0].image)+')'}}}>
+        <div id='header' class='transparent' style={{backgroundImage: 'url('+require(events[0].image)+')'}}>
           <div class='overlay'>
               <div class='row'>
                 <div class='col'>
