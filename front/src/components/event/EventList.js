53c31ce2b9a4a345964398a87d31c7b5fa4fd7df
Mluckyfield <matt.cyoshida@gmail.com>
2022-07-29 17:12:07 +0900

    add

--- front/src/components/event/EventList.js
+++ front/src/components/event/EventList.js
@@ -30,11 +30,21 @@ const EventList = () => {
   },[])
 
   const getDate=(repeats)=>{
-    const d = new Date(new Date().getYear(), new Date().getMonth() - 1, 7 * (repeats.week - 1) + 1);
-    const w = d.getDay();
-    d.setDate(d.getDate() + (7 + repeats.dow - w) % 7);
-    console.log('date',d)
-    return d;
+    let count = 0;
+    let date = new Date(new Date().getYear(),new Date().getMonth(),1)
+     while (count < repeats.week){
+         if (date.getDay() === repeats.dow){
+             break;
+         }
+         date = new Date(date.getYear() + 1900, date.getMonth(), (date.getDate() + 1));
+     }
+     console.log(date)
+     return date
+    // const d = new Date(new Date().getYear(), new Date().getMonth() - 1, 7 * (repeats.week - 1) + 1);
+    // const w = d.getDay();
+    // d.setDate(d.getDate() + (7 + repeats.dow - w) % 7);
+    // console.log('date',d)
+    // return d;
   }
 //<Carousel items={items}/>
 
