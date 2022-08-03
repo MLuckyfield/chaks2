a7a2c77186e7e405d36c410baf78b31be6cb8e26
Mluckyfield <matt.cyoshida@gmail.com>
2022-08-03 16:07:20 +0900

    add

--- models/user/api.js
+++ models/user/api.js
@@ -227,7 +227,7 @@ const moment = require ('moment')
       User.find().then((users)=>{
         console.log('cron running...',users.length)
         users.forEach((user, i) => {
-          let last = user.statistics[0]
+          let last = user.statistics.sort((a,b)=>{a.end-b.end})[0]
           if(last){
             console.log('User:',user.first,moment(last.end).format('MM D'),moment(new Date()).diff(moment(last.end),'days'))
           }
