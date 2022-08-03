e1ac0b1bef9bc4cce6864f5bb76ba422c7e891a6
Mluckyfield <matt.cyoshida@gmail.com>
2022-08-03 17:14:07 +0900

    add

--- models/user/api.js
+++ models/user/api.js
@@ -229,9 +229,12 @@ const moment = require ('moment')
         users.forEach((user, i) => {
           let last = ''
           if(user.statistics){
-            last = user.statistics.sort((a,b)=>{b.end-a.end})
-            let first = user.statistics.sort((a,b)=>{a.end-b.end})
+            last = user.statistics.sort((a,b)=>{return b.end-a.end})
+            let first = user.statistics.sort((a,b)=>{return a.end-b.end})
             console.log('User',user.first, user.statistics[0],last[0],first[0])
+          }
+          else{
+
           }
           // if(last){
           //   console.log('User:',user.first,moment(user.statistics[0].start).format('MM D'),moment(last.end).format('MM D'),moment(new Date()).diff(moment(last.end),'days'))
