c2669fa9afadc6d241f9c570920e008f77cf004f
Mluckyfield <matt.cyoshida@gmail.com>
2022-08-24 11:44:51 +0900

    add

--- front/src/components/utilities/file_handler.js
+++ front/src/components/utilities/file_handler.js
@@ -27,7 +27,7 @@ const styles = {
     height: '100%',
     justifyContent: 'center',
     padding: 20,
-  } as CSSProperties,
+  },
   file: {
     background: 'linear-gradient(to bottom, #EEE, #DDD)',
     borderRadius: 20,
@@ -38,47 +38,47 @@ const styles = {
     zIndex: 10,
     flexDirection: 'column',
     justifyContent: 'center',
-  } as CSSProperties,
+  },
   info: {
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'column',
     paddingLeft: 10,
     paddingRight: 10,
-  } as CSSProperties,
+  },
   size: {
     backgroundColor: GREY_LIGHT,
     borderRadius: 3,
     marginBottom: '0.5em',
     justifyContent: 'center',
     display: 'flex',
-  } as CSSProperties,
+  } ,
   name: {
     backgroundColor: GREY_LIGHT,
     borderRadius: 3,
     fontSize: 12,
     marginBottom: '0.5em',
-  } as CSSProperties,
+  } ,
   progressBar: {
     bottom: 14,
     position: 'absolute',
     width: '100%',
     paddingLeft: 10,
     paddingRight: 10,
-  } as CSSProperties,
+  } ,
   zoneHover: {
     borderColor: GREY_DIM,
-  } as CSSProperties,
+  } ,
   default: {
     borderColor: GREY,
-  } as CSSProperties,
+  } ,
   remove: {
     height: 23,
     position: 'absolute',
     right: 6,
     top: 6,
     width: 23,
-  } as CSSProperties,
+  } ,
 };
 
 export default function CSVReader() {
