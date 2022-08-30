25d9a4ca65600ffbd5f2ee21629d51667212f382
Mluckyfield <matt.cyoshida@gmail.com>
2022-08-30 18:07:51 +0900

    add

--- front/src/components/utilities/file_handler.js
+++ front/src/components/utilities/file_handler.js
@@ -27,14 +27,14 @@ export default class FileHandler extends Component {
         }
     });
     //result.pop()
-    console.log('Original',data)
-    axios.post(this.props.api,result)
-      .then((res) => {
-          console.log('file uploaded '+res);
-          })
-      .catch((err) => {
-        console.log('error '+err);
-        });
+    console.log('Original',data,result)
+    // axios.post(this.props.api,result)
+    //   .then((res) => {
+    //       console.log('file uploaded '+res);
+    //       })
+    //   .catch((err) => {
+    //     console.log('error '+err);
+    //     });
   }
 
   handleOnError = (err, file, inputElem, reason) => {
