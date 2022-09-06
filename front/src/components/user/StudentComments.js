047b337e32393101d023dee3b10dd7e60a72b887
Mluckyfield <matt.cyoshida@gmail.com>
2022-09-06 20:56:08 +0900

    add

--- front/src/components/user/StudentComments.js
+++ front/src/components/user/StudentComments.js
@@ -47,9 +47,9 @@ const StudentComments = () => {
           const time = moment(res.data.data.statistics[0].end).diff(moment(res.data.data.statistics[0].start), 'minutes')
           let billable = 0
           if(time-40>0){billable=time-40}
-          billable = Math.round(billable/30)*1000
+          billable = (Math.round(billable/30)*1000)+1000
           console.log('Billable time is',billable)
-          if(!status){alert(time+' minutes | '+1000+billable+'/n Start:'+moment(res.data.data.statistics[0].start).format('HH:MM')+'/n End:'+moment(res.data.data.statistics[0].end).format('HH:MM'))}
+          if(!status){alert(time+' minutes | '+billable+'/n Start:'+moment(res.data.data.statistics[0].start).format('HH:MM')+'/n End:'+moment(res.data.data.statistics[0].end).format('HH:MM'))}
         })
       .catch(error => console.log("error"+error))
   }
--- front/src/components/user/statistics.js
+++ front/src/components/user/statistics.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {axios} from "../../utilities/axios";
-import LinearProgressWithLabel from '@mui/material/Progress';
+import LinearProgressWithLabel from '@mui/material/LinearProgress';
 
 const Statistics = (props)=>{
 
