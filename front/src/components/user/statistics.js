d50697c771c96209218489317c2f1235ad240c6e
Mluckyfield <matt.cyoshida@gmail.com>
2022-09-08 12:35:40 +0900

    add

--- front/src/components/user/statistics.js
+++ front/src/components/user/statistics.js
@@ -62,7 +62,7 @@ const Statistics = (props)=>{
           :'Loading account...'}
         </div>
         Current Reward Level: {account.reward?account.reward:'Standard'}
-        {account.reward&&reward&&count?(
+        {account.reward&&reward&&count&&nextReward?(
           count>reward[account.reward][0]?<span>{reward[nextReward][0]-count} to {reward[account.reward][1]} level!</span>
             :<span>{reward[account.reward][0]-count} to keep your current status!</span>
         ):'Loading...'}
