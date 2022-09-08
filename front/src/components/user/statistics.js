9b29bf57e589cb08608b849a96efca87eb467006
Mluckyfield <matt.cyoshida@gmail.com>
2022-09-08 17:11:56 +0900

    add

--- front/src/components/user/statistics.js
+++ front/src/components/user/statistics.js
@@ -62,10 +62,10 @@ const Statistics = (props)=>{
           :'Loading account...'}
         </div>
         Current Reward Level: {account?account.reward:'Loading'}
-        {account&&reward!=undefined&&count&&nextReward?(
+        {account&&reward&&count&&nextReward?(
           count>reward[account.reward][0]?<span>{reward[nextReward][0]-count} to {reward[account.reward][1]} level!</span>
             :<span>{reward[account.reward][0]-count} to keep your current status!</span>
-        ):'Loading...'}
+        ):<span>Loading...</span>}
           <div class="progress-container">
             <div class="progress" style={{width:`${sessions}%`}}></div>{sessions}
           </div>
