30c694d74f191e97629277183d787eeef93fc1a6
Mluckyfield <matt.cyoshida@gmail.com>
2022-09-09 16:05:45 +0900

    add

--- front/src/components/user/statistics.js
+++ front/src/components/user/statistics.js
@@ -40,7 +40,7 @@ const Statistics = (props)=>{
           temp['Platinum']=[8,'Diamond']
           temp['Diamond']=[12]
           setReward(temp)
-          console.log('end of',moment(new Date()).diff(moment(new Date()).endOf('month')).format('HH:MM'))
+          console.log('end of',moment(new Date()).day()-moment(new Date()).endOf('month').day())
 
           if(user.reward){requirement=temp[user.reward][0];}
           // if(user.reward=='Gold'){setReward('Platinum');requirement=4}
