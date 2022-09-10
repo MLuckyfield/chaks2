d652081db5f387f9cda8aaa98f6d3a8cc7cb908b
Mluckyfield <matt.cyoshida@gmail.com>
2022-09-10 13:11:31 +0900

    add

--- front/src/components/user/account.js
+++ front/src/components/user/account.js
@@ -9,17 +9,53 @@ const Account = () => {
   //const [password, setPassword] = useState();
   const [student, setStudent] = useState(JSON.parse(localStorage.getItem('user')));
   const [account,setAccount]=useState()
+  const [sessions,setSessions]=useState(0)
+  // const [count,setCount]=useState(0)
+  const [reward,setReward]=useState()
+  const [msg,setMsg]=useState('')
 
   useEffect(()=>{
     // console.log('loading account view for '+JSON.stringify(student))
-    axios.get('user/all', {params:{filter:{_id:student._id}}})
+    axios.get('user/all', {params:{filter:{_id:JSON.parse(localStorage.getItem('user'))._id}}})
       .then((res) => {
           // res.data.data.forEach((item, i) => {
           //   item['inClass']=false
           // });
-          console.log(res.data.data)
-          setAccount(res.data.data[0])
-          console.log(res.data.data[0],res.data.data[0].first)
+          // console.log(res.data.data)
+          // setAccount(res.data.data[0])
+          // console.log(res.data.data[0],res.data.data[0].first)
+          let user = res.data.data[0]
+          setAccount(user)
+          res=user.statistics
+          setCount(res)
+          console.log('Statistics for',res.length)
+          let month = new Date()
+          let count = 0
+          res.forEach((item, i) => {
+            // console.log(moment(session.start).month(),month,moment(session.start).month()==month)
+            if(moment(item.start).month()==month){count++}
+          });
+          let requirement = 4
+          let temp = []
+          temp['Standard']=[0,'Gold']
+          temp['Gold']=[4,'Platinum']
+          temp['Platinum']=[8,'Diamond']
+          temp['Diamond']=[12]
+          setReward(temp)
+          console.log('end of',moment().endOf('month').diff(moment(),'days'))
+
+          if(user.reward){requirement=temp[user.reward][0];}
+          // if(user.reward=='Gold'){setReward('Platinum');requirement=4}
+          // if(user.reward=='Platinum'){setReward('Diamond');requirement=8}
+          // if(user.reward=='Diamond'){requirement=12}
+          let next = temp[user.reward][1]
+          if(count>temp[user.reward][0]){
+            setMsg(temp[next][0]-count +' more sessions to unlock '+temp[next][1]+' level!');
+            setSessions((count/temp[next][0])*100).toFixed(2)}
+          else{
+            setMsg(temp[user.reward][0]-count +' more sessions to keep your current status!');
+            setSessions((count/temp[user.reward][0]*100).toFixed(2))
+          }
         })
       .catch(error => console.log("error"+error))
   },[])
@@ -44,6 +80,14 @@ const Account = () => {
               </div>
             :'Loading account...'}
           </div>
+          {student.first=='Takeshi'?
+          <div>
+          Current Reward Level: {account?account.reward:'Loading'}
+          <span>{msg} {moment().endOf('month').diff(moment(),'days')} days left</span>
+            <div class="progress-container">
+              <div class="progress" style={{width:`${sessions}%`}}></div>{sessions}
+            </div>
+          </div>:''}
           <QRCode value={localStorage.getItem('user')}/>
       </div>
 )
