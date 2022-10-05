0ddd25b77d462b38c2f1bce4155817ecdd9761ec
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-05 12:39:55 +0900

    add

--- services/payment.js
+++ services/payment.js
@@ -137,13 +137,13 @@ router.post('/new', async (req, res)=>{
   // console.log(req.body.product)
   let line_items={
       price:req.body.product,
-      quantity:1
+      quantity:1,
+      adjustable_quantity:{enabled:true,minimum:1,maximum:100},
     }
   if(req.body.countable){line_items['adjustable_quantity']={enabled:true,minimum:1}}
   const paymentLink = await stripe.paymentLinks.create({
     line_items:[line_items],
     metadata:{order:req.body.user},
-    adjustable_quantity:{enabled:true,minimum:1,maximum:100},
     // payment_intent_data:{setup_future_usage:'off_session'},
     after_completion: {type: 'redirect', redirect: {url: 'https://chatshack.jp'}},
   })
