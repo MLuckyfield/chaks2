d4a15c6564af8d9c4de02ed3edc9d15378d43d0a
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-13 18:58:23 +0900

    add

--- models/user/api.js
+++ models/user/api.js
@@ -294,7 +294,6 @@ const { Server } = require("socket.io");
 
     //Get
     router.get('/update_goals', auth.auth, async (req, res) => {
-
       console.log('running update_goals',typeof req.query.find)
       let data = await User.findOneAndUpdate(JSON.parse(req.query.filter),JSON.parse(req.query.data),JSON.parse(req.query.find))
       // console.log('data retrieved:',data)
