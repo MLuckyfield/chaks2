60dbf85eec22d0767bc17f0d481b476432600450
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-13 21:11:38 +0900

    add

--- front/src/components/user/statistics.js
+++ front/src/components/user/statistics.js
@@ -29,8 +29,8 @@ const Statistics = (props)=>{
           let grammar_track=0
           let idiom_track=0
           res.data.data[0].progress.forEach((item, i) => {
-            if(item.ref.category=='grammar'){grammar.push(item);if(item.ref.complete){grammar_track++}}
-            if(item.ref.category=='idiom'){idioms.push(item);if(item.ref.complete){idiom_track++}}
+            if(item.ref.category=='grammar'){grammar.push(item);if(item.complete){grammar_track++}}
+            if(item.ref.category=='idiom'){idioms.push(item);console.log(item.complete);if(item.complete){idiom_track++}}
           });
           matchGoals(res.data.data[0].progress,res.data.data[0].goals)
 
