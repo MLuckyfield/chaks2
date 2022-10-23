a3a2d89e793753da4916c837108ec9fb8fe607c6
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-23 20:36:58 +0900

    add

--- front/src/components/nav/Navbar.js
+++ front/src/components/nav/Navbar.js
@@ -63,7 +63,6 @@ const Navbar = ()=>{
               {user.role=='manager'?(<li style={{padding:'3% 7%'}}><a href='/qr-reader'><span class="material-icons">qr_code_scanner</span></a></li>):''}
               {user.role=='user'?(<li style={{padding:'3% 7%'}}><a href='/dash'><span class="material-icons">home</span></a></li>):''}
               {user.role=='user'?(<li style={{padding:'3% 7%'}}><a href='/course'><span class="material-icons">trending_up</span></a></li>):''}
-              {user.first=='student'?(<li style={{padding:'3% 7%'}}><a href='/products'><span class="material-icons">home</span></a></li>):''}
               {user.role=='user'?(<li style={{padding:'3% 7%'}}><a href='/blog'><span class="material-icons">school</span></a></li>):''}
               {user.role=='user'?(<li style={{padding:'3% 7%'}}><a href='/events'><span class="material-icons">info</span></a></li>):''}
               {user.role=='user'?(<li style={{padding:'3% 7%'}}><a href='/account'><span class="material-icons">qr_code_scanner</span></a></li>):''}
