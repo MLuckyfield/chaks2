7ab7e1f41351f83d0f44f316bebd4810bee1e6c6
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-26 16:29:17 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -74,6 +74,17 @@ const TestProp = () => {
           </div>
         </div>
       </div>
+      <div class='col'>
+        <h2>Lots to Do!</h2>
+        <div class='row'>
+          <div class='col'>
+            <h2>Events</h2>
+          </div>
+          <div class='col'>
+            <h2>Guided Course</h2>
+          </div>
+        </div>
+      </div>
     </div>
 )
 }
