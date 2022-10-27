af4259802feff5d4fbcdf55c26b27d47eccf9f26
Mluckyfield <matt.cyoshida@gmail.com>
2022-10-27 13:08:59 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -10,7 +10,7 @@ const TestProp = () => {
     <div>
       <ReactPlayer url='https://www.youtube.com/watch?v=qgLZwUiLfAs' playing={true} volume={0} muted={true} width={'100%'} height={'60vh'} />
       <div id='intro' class='master-row' style={{background:'white'}}>
-        <div class='col' style={{backgroundImage: 'url('+party+')'}}>
+        <div class='col' style={{backgroundImage: 'url('+party+')',backgroundSize:'cover',backgroundPosition:'center center'}}>
             <div class='col slim center'>
                 <h1>他の英会話教室とは一味違う</h1>
             </div>
