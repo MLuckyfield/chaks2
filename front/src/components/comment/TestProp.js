b045e2ed6857d49d8df7f0e93d565d3f850123b2
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-01 14:15:06 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -88,12 +88,12 @@ const TestProp = () => {
         <h2>Lots to Do!</h2>
         <div class='row'>
           <div class='col' style={{height:'50vh',backgroundImage: 'url('+event_game+')',backgroundSize:'cover',backgroundPosition:'center center',padding:'0',width:'100%'}}>
-            <div class='mini_overlay' style={{backgroundColor:'rgba(188,151,151,0.7)',display:'flex',alignItems:'center'}}>
+            <div class='mini_overlay' style={{backgroundColor:'rgba(188,151,151,0.7)',display:'flex',alignItems:'center',justifyContent:'center'}}>
               <h2>Events</h2>
             </div>
           </div>
           <div class='col' style={{height:'50vh',backgroundImage: 'url('+event_game+')',backgroundSize:'cover',backgroundPosition:'center center',padding:'0',width:'100%'}}>
-            <div class='mini_overlay' style={{backgroundColor:'rgba(188,151,151,0.7)',display:'flex',alignItems:'center'}}>
+            <div class='mini_overlay' style={{backgroundColor:'rgba(188,151,151,0.7)',display:'flex',alignItems:'center',justifyContent:'center'}}>
               <h2>Guided Course</h2>
             </div>
           </div>
