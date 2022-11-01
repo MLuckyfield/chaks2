f9bf5ce05fbc169548a6e23339e79bf1005eb85c
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-01 18:05:31 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -56,7 +56,7 @@ const TestProp = () => {
             <div class='col' style={{height:'20%',backgroundColor:'#53cfe9',margin:'0',color:'white',padding:'6%'}}>
               <h2>COMMITMENT</h2>
             </div>
-            <div class='col' style={{height:'80%',backgroundColor:'#20bada',margin:'0',color:'white',padding:'6%'}}>
+            <div class='col' style={{height:'30vh',backgroundColor:'#20bada',margin:'0',color:'white',padding:'6%'}}>
               最初に、月に何時間勉強したいを決めて、コミット！
               <hr/>
               <p>
@@ -67,7 +67,7 @@ const TestProp = () => {
             <div class='col' style={{height:'20%',backgroundColor:'#4484c1',margin:'0',color:'white',padding:'6%'}}>
               <h2>CONSISTENCY</h2>
             </div>
-            <div class='col' style={{height:'80%',backgroundColor:'#3722aa',margin:'0',color:'white',padding:'6%'}}>
+            <div class='col' style={{height:'30vh',backgroundColor:'#3722aa',margin:'0',color:'white',padding:'6%'}}>
               自分が好きな時に、自由にご来店、 習慣化を図ろう！
               <hr/>
               <p>
@@ -78,7 +78,7 @@ const TestProp = () => {
             <div class='col' style={{height:'20%',backgroundColor:'#f75c70',margin:'0',color:'white',padding:'6%'}}>
               <h2>REWARD</h2>
             </div>
-            <div class='col' style={{height:'80%',backgroundColor:'#dc4a5e',margin:'0',color:'white',padding:'6%'}}>
+            <div class='col' style={{height:'30vh',backgroundColor:'#dc4a5e',margin:'0',color:'white',padding:'6%'}}>
               英語力を向上しながら、 ボーナスリワードも ゲット！
               <hr/>
               <p>
