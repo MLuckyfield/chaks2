ef6c5d352f64e13c8ce0cdaaf38dbeca33bbd0cc
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-02 17:05:20 +0900

    add

--- front/src/App.js
+++ front/src/App.js
@@ -132,6 +132,7 @@ const App = () => {
               if(!!error){
                 console.log('oops',error)
               }
+              
             }} constraints={{facingMode:'environment'}}/>} fail={()=><Redirect to='/login'/>}/>
             <SecureRoute path="/qr-code" access={['user','teacher','manager']} success={()=><QRCode value={localStorage.getItem('user')}/>} fail={()=><Redirect to='/login'/>}/>
       </Router>
