cc05f40861235605cd0bbd23c8ca910dc156577b
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-17 20:05:00 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useEffect } from 'react';
 import {axios} from "../../utilities/axios";
 import moment from "moment"
 import Booking from "../utilities/booking";
@@ -8,9 +8,15 @@ import Calendar from 'react-calendar'
 
 const TestProp = () => {
 
-  const [month, setMonth]=useState(()=>{let time = new Date();return time.getMonth()})
+  const [month, setMonth]=useState(()=>{let time = new Date();return time.getMonth()+1})
   const [date,setDate] = useState(()=>{let time = new Date();time.setDate(time.getDate()+2);return time})
+  const [days,setDays]=useState()
 
+  useEffect(()=>{
+    let current = new Date()
+    let d = new Date(current.getYear(),current.getMonth(),0)
+    setMonth(d)
+  },[])
 
   return(
     <div class='row'>
