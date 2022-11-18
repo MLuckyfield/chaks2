7dc1e93e17820caccb73231b285b0617d6b1d777
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-18 16:13:17 +0900

    add

--- front/src/components/page/onlineLandingPage.js
+++ front/src/components/page/onlineLandingPage.js
@@ -8,7 +8,7 @@ const Online_Landing = () => {
       <div class='col'>
         <div class='row'>
           <div class='col'>
-            <h3>test</h3>
+            <span class="material-icons">task_alt</span><h3>test</h3>
           </div>
           <div class='col'>
             <h3>test</h3>
