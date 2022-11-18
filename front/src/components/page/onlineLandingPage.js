0da0440e9eb3711d2141938267a29582cedc4551
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-18 15:41:54 +0900

    add

--- front/src/components/page/onlineLandingPage.js
+++ front/src/components/page/onlineLandingPage.js
@@ -5,6 +5,46 @@ const Online_Landing = () => {
 
   return(
     <div>
+      <div class='col'>
+        <div class='row'>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+          <div class='col'>
+            <h3>test</h3>
+          </div>
+        </div>
+      </div>
+      <div class='col'>
+        <div class='col'>
+          <h3>step 1</h3>
+          <p>blah blah</p>
+        </div>
+        <div class='col'>
+          <h3>step 2</h3>
+          <p>blah blah</p>
+        </div>
+        <div class='col'>
+          <h3>step 3</h3>
+          <p>blah blah</p>
+        </div>
+      </div>
       <Signup redirect={'/online'}/>
     </div>
 )
