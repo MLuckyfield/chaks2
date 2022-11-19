2b28e6f52b3c7036436b69fa24ab4da42f80f8eb
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-19 19:57:10 +0900

    add

--- models/site_event/api.js
+++ models/site_event/api.js
@@ -19,9 +19,9 @@ const email = require('../../services/email')
         const time = end.diff(start, 'minutes')
         console.log('resetting, testing expiry',time)
         if(time<120){
-          User.findByIdAndUpdate({_id:site_event._id},{password:auth.newPass(req.password)})
+          User.findByIdAndUpdate({_id:req.security_code},{password:auth.newPass(req.password)})
           .then(()=>{
-            Site_Event.findByIdAndDelete({_id:site_event._id}).then(()=>{
+            Site_Event.findByIdAndDelete({_id:req.security_code}).then(()=>{
               console.log('pw reset done')
               return res.status(201).json({
                 message: 'password updated',
