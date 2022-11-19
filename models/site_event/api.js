1be42a4af464a221ffa1127366f2818b3d8edcdd
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-19 20:14:57 +0900

    add

--- models/site_event/api.js
+++ models/site_event/api.js
@@ -18,21 +18,28 @@ const email = require('../../services/email')
         let end = moment(new Date())
         const time = end.diff(start, 'minutes')
         console.log('resetting, testing expiry',time)
-        let new_pw = auth.newPass(req.password)
         if(time<120){
-          User.findByIdAndUpdate({_id:req.security_code},{password:new_pw})
-          .then(()=>{
-            Site_Event.findByIdAndDelete({_id:req.security_code}).then(()=>{
-              console.log('pw reset done')
-              return res.status(201).json({
-                message: 'password updated',
-                success: true
-              });
+          try{
+            User.findByIdAndUpdate({_id:req.security_code},{password:auth.newPass(req.password)})
+            .then(()=>{
+              Site_Event.findByIdAndDelete({_id:req.security_code}).then(()=>{
+                console.log('pw reset done')
+                return res.status(201).json({
+                  message: 'password updated',
+                  success: true
+                });
+              }).catch((err)=>{
+                console.log('site_event delete failure',err)
+              })
             }).catch((err)=>{
-              console.log('site_event delete failure',err)
+              console.log('update failure',err)
+              return res.status(400).json({
+                message: 'password update failed',
+                success: false
+              });
             })
-          }).catch((err)=>{
-            console.log('update failure',err)
+          }catch((err)=>{
+            console.log('password hash error',err)
             return res.status(400).json({
               message: 'password update failed',
               success: false
