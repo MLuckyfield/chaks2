6ad6cbf61e09ccfbbd7f1cef2da12ba26564572e
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-29 19:31:17 +0900

    add

--- front/src/components/page/onlineLandingPage.js
+++ front/src/components/page/onlineLandingPage.js
@@ -13,33 +13,35 @@ const Online_Landing = () => {
               <div class='col'>
                 <h2>CHATSHACK is now</h2>
                 <h1>ONLINE</h1>
-                <a class='solid-first cta' href="#signup">More</a>
+                <a class='solid-first cta' href="#signup" style={{textDecoration:'none',textAlign:'center'}}>MORE</a>
               </div>
             </div>
         </div>
       </div>
       <div class='col slim' style={{backgroundSize:'cover',backgroundColor:'ghostwhite',backgroundImage:'url('+environment+')',padding:'none !important',margin:'none !important',width:'100%'}}>
-        <div class='row'>
+        <div class='mini_overlay' style={{backgroundColor:'rgba(248,248,255,0.8)',display:'flex',alignItems:'center',flexDirection:'column',width:'100vw'}}>
+          <div class='row'>
+              <div class='col'>
+                <div class='fixed-row align' style={{marginBottom:'10%'}}>
+                  <span class="material-icons green ">task_alt</span><h2>ネイティブスピーカー</h2>
+                </div>
+                <div class='fixed-row align'  style={{marginBottom:'10%'}}>
+                  <span class="material-icons green ">task_alt</span><h2>マンツーマンレッスン</h2>
+                </div>
+                <div class='fixed-row align'  style={{marginBottom:'10%'}}>
+                  <span class="material-icons green ">task_alt</span><h2>優秀な講師陣</h2>
+                </div>
+            </div>
             <div class='col'>
               <div class='fixed-row align' style={{marginBottom:'10%'}}>
-                <span class="material-icons green ">task_alt</span><h2>ネイティブスピーカー</h2>
+                <span class="material-icons green ">task_alt</span><h2>オンラインフィードバック</h2>
               </div>
-              <div class='fixed-row align'  style={{marginBottom:'10%'}}>
-                <span class="material-icons green ">task_alt</span><h2>マンツーマンレッスン</h2>
+              <div class='fixed-row align' style={{marginBottom:'10%'}}>
+                <span class="material-icons green ">task_alt</span><h2>自由なレッスン</h2>
               </div>
-              <div class='fixed-row align'  style={{marginBottom:'10%'}}>
-                <span class="material-icons green ">task_alt</span><h2>優秀な講師陣</h2>
+              <div class='fixed-row align' style={{marginBottom:'10%'}}>
+                <span class="material-icons green ">task_alt</span><h2>選べるレッスン時間</h2>
               </div>
-          </div>
-          <div class='col'>
-            <div class='fixed-row align' style={{marginBottom:'10%'}}>
-              <span class="material-icons green ">task_alt</span><h2>オンラインフィードバック</h2>
-            </div>
-            <div class='fixed-row align' style={{marginBottom:'10%'}}>
-              <span class="material-icons green ">task_alt</span><h2>自由なレッスン</h2>
-            </div>
-            <div class='fixed-row align' style={{marginBottom:'10%'}}>
-              <span class="material-icons green ">task_alt</span><h2>選べるレッスン時間</h2>
             </div>
           </div>
         </div>
