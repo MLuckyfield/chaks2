ce4f829655445df342fe643882358cbe22bdd319
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-29 19:55:49 +0900

    add

--- front/src/components/page/onlineLandingPage.js
+++ front/src/components/page/onlineLandingPage.js
@@ -64,7 +64,7 @@ const Online_Landing = () => {
         </div>
       </div>
       <div class='col slim' style={{backgroundSize:'cover',backgroundColor:'ghostwhite',backgroundImage:'url('+environment+')',padding:'0 !important',margin:'0 !important',width:'100%'}}>
-        <div class='mini_overlay' style={{backgroundColor:'rgba(248,248,255,0.8)',display:'flex',alignItems:'center',flexDirection:'column',width:'100vw'}}>
+        <div class='mini_overlay' style={{backgroundColor:'rgba(248,248,255,0.8)',display:'flex',alignItems:'center',flexDirection:'column',width:'100vw',padding:'0 !important'}}>
           <div class='fixed-row' style={{width:'50%'}}>
             <h1 style={{margin:'10% 0'}}>NEXT STEPS</h1>
           </div>
