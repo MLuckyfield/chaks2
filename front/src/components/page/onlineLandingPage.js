152669b7e5c05e3f68269e3ad83b8f4537d4b41a
Mluckyfield <matt.cyoshida@gmail.com>
2022-11-30 17:13:47 +0900

    add

--- front/src/components/page/onlineLandingPage.js
+++ front/src/components/page/onlineLandingPage.js
@@ -18,6 +18,19 @@ const Online_Landing = () => {
             </div>
         </div>
       </div>
+      <div class='col slim'>
+        <div class='fixed-row' style={{width:'50%'}}>
+          <h1 style={{margin:'10% 0'}}>METHOD</h1>
+        </div>
+        <div class='row'>
+          <div class='col'>
+            INPUT
+          </div>
+          <div class='col'>
+            OUTPUT
+          </div>
+        </div>
+      </div>
       <div class='col slim' style={{backgroundSize:'cover',backgroundColor:'ghostwhite',backgroundImage:'url('+environment+')',padding:'0',margin:'0',width:'100%'}}>
         <div class='mini_overlay' style={{backgroundColor:'rgba(248,248,255,0.8)',display:'flex',alignItems:'center',flexDirection:'column',width:'100vw'}}>
           <div class='row'>
