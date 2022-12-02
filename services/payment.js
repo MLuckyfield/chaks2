bc15a93623314b514ac87830ae31dcfa99bdd1d0
Mluckyfield <matt.cyoshida@gmail.com>
2022-12-02 15:28:51 +0900

    add

--- services/payment.js
+++ services/payment.js
@@ -103,7 +103,7 @@ router.post('/complete', express.raw({type:'application/json'}),async (req, res)
           for(let i = 0;i<session.lines.data[0].quantity*2;i++){
             units.push({value:30})
           }
-          purchased = {$push:{points:{$each:units}}}
+          purchased = {$set:{points:units}}
           console.log('will add',units)
           updateUser(identifier,purchased,res)
           break;
