15bd47b6a789e594444d95fd7a4ea2a902141ea4
Mluckyfield <matt.cyoshida@gmail.com>
2022-12-07 13:56:31 +0900

    add

--- front/src/components/user/Signup.js
+++ front/src/components/user/Signup.js
@@ -26,7 +26,11 @@ const onSubmit = (e) => {
       // console.log('response'+res)
       localStorage.setItem('user', JSON.stringify(res.data.result));
       setMsg([res.data.message,res.data.success]);
-      window.location=props.redirect
+      if(props.redirect.slice(0,4)='http'){
+        window.location.href=props.redirect
+      }else{
+        window.location=props.redirect
+      }
     })
     .catch((err) => {
       setForm(true)
