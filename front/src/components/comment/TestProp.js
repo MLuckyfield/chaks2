0a38a30b41ddc30887c9ba7fa83e49b927eaab5e
Mluckyfield <matt.cyoshida@gmail.com>
2022-12-08 19:35:52 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -24,7 +24,8 @@ const TestProp = () => {
   },[])
   const buildCalendar = ()=>{
     let startingDay = new Date().getDay()
-    let endingDay = days.getDay()
+    let endingDay = new Date(new Date().getYear(),new Date().getMonth(),0).getDate()+1
+    console.log(startingDay,endingDay)
     for(let i=1;i<days+(6-endingDay)+startingDay;i++){
       return <div class='dayBox'>{i}</div>
     }
