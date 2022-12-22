9ae8f19ead68bb7c01ec6695b6e89660ba91e96f
Mluckyfield <matt.cyoshida@gmail.com>
2022-12-22 15:44:56 +0900

    add

--- front/src/App.js
+++ front/src/App.js
@@ -278,7 +278,17 @@ const Front = ()=>{
       // </div>
       return (
         <div>
-              <ReactPlayer url='https://www.youtube.com/watch?v=qgLZwUiLfAs' playing={true} volume={0} muted={true} width={'100%'} height={'60vh'} playIcon={<div>HELLO</div>}/>
+        <ReactPlayer
+            url='https://www.youtube.com/watch?v=qgLZwUiLfAs'
+            playing={true} volume={0} muted={true} width={'100%'} height={'60vh'}
+            playIcon={
+              <div class='mini_overlay' style={{backgroundColor:'rgba(0,0,0,0.3)',display:'flex',alignItems:'center'}}>
+                <div class='col'>
+                  <h1 style={{margin:'2%'}}>楽しく身につく<br/>新しい形の英会話</h1>
+                  <button style={{width:'50%',color:'white',backgroundColor:'black', margin:'2%'}}>コンセプト見る</button>
+                </div>
+              </div>
+            } light={banner}/>
               <div id='concept' class='master-row' style={{background:'white',color:'white',paddingTop:'0'}}>
                 <div class='col' style={{backgroundImage: 'url('+atmos+')',backgroundSize:'cover',backgroundPosition:'center center',padding:'0',width:'100%'}}>
                     <div class='mini_overlay col' style={{backgroundColor:'rgba(0,0,0,0.3)',width:'100%',margin:'0'}}>
