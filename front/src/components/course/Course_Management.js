5a87a148351182ecc2c4d21db27ffb95c53aa34b
Mluckyfield <matt.cyoshida@gmail.com>
2023-01-26 18:41:38 +0900

    add

---
+++ front/src/components/course/Course_Management.js
@@ -0,0 +1,20 @@
+import React, { useRef, useState, useEffect } from 'react';
+import {axios} from "../../utilities/axios";
+import moment from "moment"
+import Popup from '../utilities/popup'
+import Accordion from '../utilities/accordion'
+
+
+const Course_Management = () => {
+
+  useEffect(()=>{
+
+  },[])
+  return(
+      <div class='row'>
+      
+      </div>
+)
+}
+
+export default Course_Management;
