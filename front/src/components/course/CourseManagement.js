f6e5122a319e38fa320df6fe6d728ddd4b474755
Mluckyfield <matt.cyoshida@gmail.com>
2023-01-27 15:53:15 +0900

    add

--- front/src/components/course/CourseManagement.js
+++ front/src/components/course/CourseManagement.js
@@ -138,7 +138,7 @@ const AccordionItem=(props)=>{
         {isActive &&
           <div class='accordion-content'>
             <div class='col slim'>
-            <EditorView content={course.description} readOnly={true}/>
+            test
             </div>
           </div>}
       </div>
