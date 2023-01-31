6cd860261051046759147085cab86bb990e33738
Mluckyfield <matt.cyoshida@gmail.com>
2023-01-31 16:31:50 +0900

    add

--- models/booking/api.js
+++ models/booking/api.js
@@ -73,8 +73,8 @@ router.post('/delete', async (req, res) => {
 // //create new
 router.post('/create', async (req, res) => {
   let data = req.body
-  let new_slot = moment(new Date(new Date().getYear(),data.month,data.date)).set({h:data.hour,m:data.minute})
-  console.log('booking update request',data.month,data.date,new Date().getYear(),new_slot)
+  let new_slot = moment(new Date(new Date().getFullYear(),data.month,data.date)).set({h:data.hour,m:data.minute})
+  console.log('booking update request',data.month,data.date,new Date().getFullYear(),new_slot)
 
   await Booking.insertMany({
       date: moment.utc(new_slot).toDate(),
