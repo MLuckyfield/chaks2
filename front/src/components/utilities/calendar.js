1b696cfb9f1479b593d0c781b90109363990b10f
Mluckyfield <matt.cyoshida@gmail.com>
2023-01-31 16:57:10 +0900

    add

--- front/src/components/utilities/calendar.js
+++ front/src/components/utilities/calendar.js
@@ -144,7 +144,7 @@ const Calendar = () => {
             return (<div class={new Date(year,month-1,item.day).getDate()>=today.getDate()?'dayBox':'dayBox inactive'}>
                     {moment(new Date(year,month-1,item.day)).format('MM Do YY')==moment(today).format('MM Do YY')?<span class='day_tag' style={{color:'white',backgroundColor:'tomato'}}>{item.day}</span>:<span class='day_tag'>{item.day}</span>}
                     {item.bookings.map((timeslot,y)=>{
-                      return <Lesson title={`${timeslot.teacher.first} | ${displayTime(timeslot.date.hour(),timeslot.date.minute())}`} num={timeslot.date} time={displayTime(timeslot.date.hour(),timeslot.date.minute())} content={timeslot}/>
+                      return <Lesson title={`${timeslot.teacher.first?timeslot.teacher.first:''} | ${displayTime(timeslot.date.hour(),timeslot.date.minute())}`} num={timeslot.date} time={displayTime(timeslot.date.hour(),timeslot.date.minute())} content={timeslot}/>
                     })}
                    </div>)
           }
