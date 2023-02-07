13f5e81b390c8e584376c30b69b63570e3f3196b
Mluckyfield <matt.cyoshida@gmail.com>
2023-02-07 21:23:02 +0900

    add

--- front/src/components/course/CourseManagement.js
+++ front/src/components/course/CourseManagement.js
@@ -322,6 +322,9 @@ const AccordionItem=(props)=>{
     next_start.hours(schedule.timeslots[0].start_hour)
     next_start.minutes(schedule.timeslots[0].start_minute)
     let firstday = moment().month(next_start.month()).startOf('month').isoWeekday(schedule.timeslots[0].day)
+    if(firstday.date() > 7){
+      firstday=firstday.isoWeekday(-6)
+    }
     console.log('firstday',firstday)
     next_start.day(schedule.timeslots[0].day)
     // next_start = moment(new Date(moment().year(),next_start,1))
