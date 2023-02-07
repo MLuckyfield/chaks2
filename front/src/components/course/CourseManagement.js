92d561551d4d8a93cbba884dd97c098a12fd274b
Mluckyfield <matt.cyoshida@gmail.com>
2023-02-07 22:56:12 +0900

    add

--- front/src/components/course/CourseManagement.js
+++ front/src/components/course/CourseManagement.js
@@ -331,7 +331,7 @@ const AccordionItem=(props)=>{
     // next_start = moment(new Date(moment().year(),next_start,1))
     // console.log('new calc',starting_month,current_month,gap,cycles,next_start)
     next_start={
-      start:next_start.format('M/D, ddd@HH:mm'),
+      start:next_start.format('M/D'),
       time:next_start.format('ddd@HH:mm'),
       graduation:next_start.add(repeats,'months').format('M/D'),
       limit:schedule.timeslots[0].limit,
