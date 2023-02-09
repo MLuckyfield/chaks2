22644ce3f1dc453304b0ca7de0d421370e926f79
Mluckyfield <matt.cyoshida@gmail.com>
2023-02-09 19:29:56 +0900

    add

--- front/src/components/utilities/calendar.js
+++ front/src/components/utilities/calendar.js
@@ -161,7 +161,7 @@ const Calendar = () => {
                     })}
                    </div>)
           }
-          else if (user.role=='student') {
+          else if (user.role=='user') {
             return (<div class={new Date(year,month-1,item.day).getDate()>=today.getDate()?'dayBox':'dayBox inactive'}>
                     {moment(new Date(year,month-1,item.day)).format('MM Do YY')==moment(today).format('MM Do YY')?<span class='day_tag' style={{color:'white',backgroundColor:'tomato'}}>{item.day}</span>:<span class='day_tag'>{item.day}</span>}
                     {item.bookings.map((timeslot,y)=>{
