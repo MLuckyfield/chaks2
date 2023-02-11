7a0a980e1f0608c23b38646dcd1433913fc8f907
Mluckyfield <matt.cyoshida@gmail.com>
2023-02-11 18:51:08 +0900

    add

--- front/src/components/nav/AdminDash.js
+++ front/src/components/nav/AdminDash.js
@@ -183,7 +183,7 @@ const Dash = ()=>{
       <div>
         <StaffTable/>
         <Table name='Teachers' api='/user/all' filter={{role: 'teacher'}} fields="-__v -fluency -online_schedule -online_slots -progress -goals -students -tags -source -password -createdAt -updatedAt -points -active -statistics -plan -reward -subscriptions"/>
-        <Table name='New This Month' api='/user/all' filter={{createdAt:{$gte:new Date(`${year}-${month}-1`),$lte:new Date(`${year}-${month}-${0}`)}}} fields="-__v -fluency -progress -online_schedule -online_slots -goals -students -tags -source -password -createdAt -updatedAt -role -active -statistics -subscriptions"/>
+        <Table name='New This Month' api='/user/all' filter={{createdAt:{$gte:new Date(`${year}-${month}-1`),$lte:new Date(`${year}-${month}-${0}`).getDate()}}} fields="-__v -fluency -progress -online_schedule -online_slots -goals -students -tags -source -password -createdAt -updatedAt -role -active -statistics -subscriptions"/>
 
         <StudentTable/>
       </div>
