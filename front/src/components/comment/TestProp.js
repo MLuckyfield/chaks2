9b206a83023747ca8eac8f4970823a8398928dc5
Mluckyfield <matt.cyoshida@gmail.com>
2023-02-12 17:29:39 +0900

    add

--- front/src/components/comment/TestProp.js
+++ front/src/components/comment/TestProp.js
@@ -4,10 +4,31 @@ import moment from "moment-timezone"
 import Course from '../page/course'
 
 const TestProp = () => {
+  const [account,setAccount]=useState(JSON.parse(localStorage.getItem('user')))
+  const [courses,setCourses]=useState(()=>{
+    axios.get('/enrolled/all',{params:{filter:{student:account._id}}})
+      .then((res) => {
+          setCourses(res.data.data)
+          })
+      .catch((err) => {
+        console.log(err);
+        });
+  })
   return (
-      <div class='row'>
-        <Course/>
-      </div>
+    <div class='col border'>
+        <h1>ACCOUNT</h1>
+        {account?
+        <div class='col'>
+          Subscriptions:{account.subscriptions.length>0?account.subscsriptions.map((sub,i)=>{
+            return <div class='row'>{sub.name} {moment(sub.start).format('M/D')}</div>
+          }):'You are not subscribed!'}
+          ポイント: 20
+        </div>
+      :'Loading account...'}
+      {courses?courses.map(function(course,i){
+        return <div>{course.course.name} {course.status}</div>
+      }):''}
+    </div>
   )
 }
 
