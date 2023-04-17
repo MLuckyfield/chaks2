a18fd7dc7396bdef60b4a00d77b27167bbcb6239
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-18 00:17:45 +0900

    add

--- front/src/components/comment/CommentV2.js
+++ front/src/components/comment/CommentV2.js
@@ -7,13 +7,13 @@ import * as constants from '../../utilities/constants'
 const Comment = (props) => {
 
   const [comment, setcomment] = useState(props.comment);
-  const commentContent = useRef({value:props.comment.hasOwnProperty('comment')?props.comment.comment:''});
+  const commentContent = useRef({value:''});
   const [active,setActive]=useState(true)
   const [user,setUser]=useState(getCurrentUser())
 
   useEffect(()=>{
     console.log('comment recieved',props.comment.hasOwnProperty('comment'),comment, commentContent)
-    // commentContent.current.value=props.comment.hasOwnProperty('comment')?props.comment.comment:''
+    commentContent.current.value=props.comment.hasOwnProperty('comment')?props.comment.comment:''
   },[])
 
   const draftComment = (commentId, e) => {
