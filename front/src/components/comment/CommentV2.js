90212f649b8f6adb91ddda46469fae05a9ea58d6
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-17 17:19:49 +0900

    add

--- front/src/components/comment/CommentV2.js
+++ front/src/components/comment/CommentV2.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import {axios} from "../../utilities/axios";
 import moment from "moment"
 import {checkPermission,getCurrentUser} from '../../utilities/helpers'
