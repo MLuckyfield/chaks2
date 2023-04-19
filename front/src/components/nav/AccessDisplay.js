2ed98cfde010ad888a10fe11c354349d9b5a073f
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-19 14:14:06 +0900

    add

--- front/src/components/nav/AccessDisplay.js
+++ front/src/components/nav/AccessDisplay.js
@@ -52,7 +52,7 @@ import GoogleMapReact from 'google-map-react'
   },[])
   return (
   <div id='map'>
-      {location?<GoogleMapReact bootstrapURLKeys={{key:'AIzaSyBX-HH0dhkemDet_G5TTZsR__uphcOEI6k'}} defaultCenter={location} defaultZoom={14} v={3.31}>
+      {location?<GoogleMapReact bootstrapURLKeys={{key:process.env.GOOGLE_MAP_API}} defaultCenter={location} defaultZoom={14} v={3.31}>
           <LocationPin lat={location.lat} lng={location.lng} text='ChatShack'/>
       </GoogleMapReact>:'loading'}
   </div>
