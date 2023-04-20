95648f08685cf0e9ea613fd0560e1e2570471132
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-20 21:33:15 +0900

    add

--- front/src/components/comment/CommentV2.js
+++ front/src/components/comment/CommentV2.js
@@ -55,20 +55,23 @@ const Comment = (props) => {
   return(
     <div class='col feedback'>
         <div class='col'>
-          {comment.status=='approved'?(
-            checkPermission(user.role,constants.TEACHER)?
-            <form onSubmit={(e)=>draftComment(comment._id,e)} style={{width:'80%'}}>
-            <h2>New Comment</h2>
-            <h3>{comment.status}</h3>
-            <div>
-              <textarea ref={commentContent} type="text" class="form-control" placeholder="Comment: make sure to include 1) encouragement (1+ things they did well) 2) key topics you discussed 3) improvement points/English things you explained" required/>
-            </div>
-            <div>
-              <input type="text" class="form-control" placeholder={`${props.student.first} ${props.student.last}`} disabled/>
-            </div>
-            {active?<button type="submit" class="solid-first">Comment</button>:'Please wait... (manually refresh after 5 seconds)'}
-            </form>:'Feedback in progress - please check within 24 hours!'
-          ):comment.comment}
+          {comment.status=='approved'?comment.comment:
+          (
+            comment.status=='draft'?'Your feedback is being preapred - please check within 24 hours!'
+            // checkPermission(user.role,constants.TEACHER)?
+            // <form onSubmit={(e)=>draftComment(comment._id,e)} style={{width:'80%'}}>
+            // <h2>New Comment</h2>
+            // <h3>{comment.status}</h3>
+            // <div>
+            //   <textarea ref={commentContent} type="text" class="form-control" placeholder="Comment: make sure to include 1) encouragement (1+ things they did well) 2) key topics you discussed 3) improvement points/English things you explained" required/>
+            // </div>
+            // <div>
+            //   <input type="text" class="form-control" placeholder={`${props.student.first} ${props.student.last}`} disabled/>
+            // </div>
+            // {active?<button type="submit" style={{width:'250px'}} class="solid-first">Comment</button>:'Please wait... (manually refresh after 5 seconds)'}
+            // </form>
+            :'Feedback in progress - please check within 24 hours!'
+          )}
         </div>
         <div class="chip">
           <img src={constants.PROFILES[`_${comment.author._id}`]} alt="Person" width="96" height="96"></img>
