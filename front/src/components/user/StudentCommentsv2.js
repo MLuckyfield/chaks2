3347d7e0ba351b147a580a6a98255cd9eadd3a3b
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-21 13:12:23 +0900

    add

--- front/src/components/user/StudentCommentsv2.js
+++ front/src/components/user/StudentCommentsv2.js
@@ -138,13 +138,13 @@ const manualComment = (teacherId)=>{
           </div>
           <Popup button={"Add Comment"} num={3} content={
             <div class='col'>
-              <button onClick={()=>startSession('62fb3ed3bc7766179393b277')} class='button'>Vincent</button>
-              <button onClick={()=>startSession('63882dbd8a0031a501d54140')} class='button'>Radka</button>
-              <button onClick={()=>startSession('640d4ff6470b0e234739c640')} class='button'>Liza</button>
-              <button onClick={()=>startSession('64327746ee94db5a26b715c0')} class='button'>Mimmi</button>
-              <button onClick={()=>startSession('6432522fee94db5a26b6291b')} class='button'>Momo</button>
-              <button onClick={()=>startSession('641129d948fed7fcee0cf312')} class='button'>Futaba</button>
-              <button onClick={()=>startSession('628f3e7b8981f84051396159')} class='button'>Shunsuke</button>
+              <button onClick={()=>manualComment('62fb3ed3bc7766179393b277')} class='button'>Vincent</button>
+              <button onClick={()=>manualComment('63882dbd8a0031a501d54140')} class='button'>Radka</button>
+              <button onClick={()=>manualComment('640d4ff6470b0e234739c640')} class='button'>Liza</button>
+              <button onClick={()=>manualComment('64327746ee94db5a26b715c0')} class='button'>Mimmi</button>
+              <button onClick={()=>manualComment('6432522fee94db5a26b6291b')} class='button'>Momo</button>
+              <button onClick={()=>manualComment('641129d948fed7fcee0cf312')} class='button'>Futaba</button>
+              <button onClick={()=>manualComment('628f3e7b8981f84051396159')} class='button'>Shunsuke</button>
             </div>
           }/>
           <div class='col'>
