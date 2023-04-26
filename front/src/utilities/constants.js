67c2c88678d724b0195e6abb55e9b1a82b3493ae
Mluckyfield <matt.cyoshida@gmail.com>
2023-04-26 12:13:07 +0900

    add

--- front/src/utilities/constants.js
+++ front/src/utilities/constants.js
@@ -9,7 +9,7 @@ import mimmi from '../images/staff/mimmi.jpg'
 import bre from '../images/staff/bre.jpg'
 import momo from '../images/staff/momo.jpg'
 import futaba from '../images/staff/futaba.jpg'
-export const PROFILES = {
+export const PROFILESv2 = {
         _62fb3ed3bc7766179393b277:{image:vincent,name:'Vincent',active:true},
         _63882dbd8a0031a501d54140:{image:radka,name:'Radka',active:true},
         _63e718f7ccf275b1babaa770:{image:cedric,name:'Cedric',active:false},
@@ -22,19 +22,19 @@ export const PROFILES = {
         _641129d948fed7fcee0cf312:{image:futaba,name:'Futaba',active:true},
 
 }
-// export const PROFILES = {
-//         _62fb3ed3bc7766179393b277:vincent,
-//         _63882dbd8a0031a501d54140:radka,
-//         _63e718f7ccf275b1babaa770:cedric,
-//         _63218b02f17f3f46bf91af22:bre,
-//         _628c7aefc378ce2a01df6ac7:matt,
-//         _628f3e7b8981f84051396159:shunsuke,
-//         _640d4ff6470b0e234739c640:liza,
-//         _64327746ee94db5a26b715c0:mimmi,
-//         _6432522fee94db5a26b6291b:momo,
-//         _641129d948fed7fcee0cf312:futaba,
-//
-// }
+export const PROFILES = {
+        _62fb3ed3bc7766179393b277:vincent,
+        _63882dbd8a0031a501d54140:radka,
+        _63e718f7ccf275b1babaa770:cedric,
+        _63218b02f17f3f46bf91af22:bre,
+        _628c7aefc378ce2a01df6ac7:matt,
+        _628f3e7b8981f84051396159:shunsuke,
+        _640d4ff6470b0e234739c640:liza,
+        _64327746ee94db5a26b715c0:mimmi,
+        _6432522fee94db5a26b6291b:momo,
+        _641129d948fed7fcee0cf312:futaba,
+
+}
 //permissions
 export const USER = 0;
 export const TEACHER = 1;
